567a11c3d7d4d7df388d96856e46ba4ed6cba71f
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-03-18 10:50:23 +0100

    add constructor to Person class

--- src/main/java/it/unibo/wastemaster/core/models/Person.java
+++ src/main/java/it/unibo/wastemaster/core/models/Person.java
@@ -1,5 +1,20 @@
 package it.unibo.wastemaster.core.models;
 
-public class Person {
+public abstract class Person {
+    protected int id;
+    protected String name;
+    protected String address;
+    protected String email;
+    protected String phone;
+
+
+    // Constructor class Person
+    public Person(int id, String name, String address, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
     
 }
