10dd9358e12e933f016f6f35ead407cc76576b96
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-03-18 17:21:35 +0100

    Fix app.java and add Employees.java

--- src/main/java/it/unibo/wastemaster/main/App.java
+++ src/main/java/it/unibo/wastemaster/main/App.java
@@ -1,14 +1,8 @@
 package it.unibo.wastemaster.main;
 
-import it.unibo.wastemaster.core.models.Person;
-import it.unibo.wastemaster.core.models.Customer;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Person testPerson = new Person(1, "Mario", "Via roma 27", "mario@example.com", "1234567890") {};
-        System.out.println("Person: " + testPerson.getInfo());
-        Customer testClient = new Customer(2, "Luigi", "Via milano 12", "luigi@example.com", "0987654321", "17") {};
-        System.out.println("Client: " + testClient.getInfo());
     }
 }
