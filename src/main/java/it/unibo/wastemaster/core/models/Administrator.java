b84c572cfc7c63d559a29d6860e612256b942e54
Alex <alex.cambrini@studio.unibo.it>
2025-03-22 18:06:36 +0100

    update constructor

--- src/main/java/it/unibo/wastemaster/core/models/Administrator.java
+++ src/main/java/it/unibo/wastemaster/core/models/Administrator.java
@@ -1,26 +1,32 @@
 package it.unibo.wastemaster.core.models;
 
-public class Administrator extends Employees {
+public class Administrator extends Employee {
 
-    // Constructor class Administrator
-    public Administrator(int id, String name, String address, String email, String phone, int employeeId) {
-        super(id, name, address, email, phone, employeeId, "Administrator");
+    public Administrator(int id, String name, String surname, Location address, String email, String phone, int employeeId) {
+        super(id, name, surname, address, email, phone, employeeId, Role.Administrator);
     }
 
-    //method to manage users
-    public void manageUsers() {
-        System.out.println("Manage Users...");
-    }
-
-    //method to manage roles
-    public void manageRoles() {
-        System.out.println("Manage Roles...");
-    }
-
-    // Add getInfo method to return all the attributes of the class Administrator
     @Override
     public String getInfo() {
-        return super.getInfo() + ", (Privileges Administrator)";
+        return super.getInfo();
     }
+
+    // public static void main(String[] args) {
+    //     // Crea un oggetto Location per testare l'indirizzo
+    //     Location location = new Location(1, "Via Roma", "15", "Milano", "Italy");
+
+    //     Administrator admin = new Administrator(1, "Mario", "Rossi", location, "mario.rossi@example.com", "1234567890", 1001);
+
+    //     System.out.println(admin.getInfo());
+    //     admin.setPhone("55555");
+        
+    //     System.out.println("ID: " + admin.getId());
+    //     System.out.println("Name: " + admin.getName());
+    //     System.out.println("Surname: " + admin.getSurname());
+    //     System.out.println("Email: " + admin.getEmail());
+    //     System.out.println("Phone: " + admin.getPhone());
+    //     System.out.println("Employee ID: " + admin.getEmployeeId());
+    //     System.out.println("Role: " + admin.getRole());
+    // }
     
 }
