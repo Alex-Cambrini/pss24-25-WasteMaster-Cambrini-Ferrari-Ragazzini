5bdea2371ea6790c6a1a7a5e2530fd9f1dde3783
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-03-22 10:55:09 +0100

    Add vehicle class

---
+++ src/main/java/it/unibo/wastemaster/core/models/Vehicle.java
@@ -0,0 +1,36 @@
+package it.unibo.wastemaster.core.models;
+
+public class Vehicle {
+    private int id;
+    private String type;
+    private String status;
+
+    // Constructor for Vehicle
+    public Vehicle(int id, String type, String status) {
+        this.id = id;
+        this.type = type;
+        this.status = status;
+    }
+
+    // add get for the attributes of the class Vehicle
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // add set for the attributes of the class Vehicle
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
