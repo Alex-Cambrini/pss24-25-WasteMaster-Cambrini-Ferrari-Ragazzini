aa1d1fbc2389ff8f1d701dca7d885100a78c8b4d
Alex <alex.cambrini@studio.unibo.it>
2025-03-22 18:11:20 +0100

    update constructor

--- src/main/java/it/unibo/wastemaster/core/models/Operator.java
+++ src/main/java/it/unibo/wastemaster/core/models/Operator.java
@@ -1,14 +1,21 @@
 package it.unibo.wastemaster.core.models;
+public class Operator extends Employee {
 
-public class Operator extends Employees {
-    // Constructor for Operator
-    public Operator(int id, String name, String address, String email, String phone, int employeeId) {
-        super(id, name, address, email, phone, employeeId, "Operator");
+    public Operator(int id, String name, String surname, Location address, String email, String phone, int employeeId) {
+        super(id, name, surname, address, email, phone, employeeId, Role.Operator);
     }
 
-    // Add getInfo method to return all the attributes of the class Operator
     @Override
     public String getInfo() {
-        return super.getInfo() + ", (Privileges Operator)";
+        return super.getInfo();
     }
+
+    // //TEST
+    // public static void main(String[] args) {
+    //     Location location = new Location(1, "Via Torino", "30", "Milano", "Italy");
+    //     Operator operator = new Operator(2, "Giovanni", "Bianchi", location, "giovanni.bianchi@example.com", "9876543210", 2001);    
+    //     System.out.println("Info iniziali: " + operator.getInfo());   
+    //     operator.setPhone("0123456789");
+    //     System.out.println("Info dopo la modifica del telefono: " + operator.getInfo());
+    // }
 }
