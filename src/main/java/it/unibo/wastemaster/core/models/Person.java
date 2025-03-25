20ea04a308adcebee9d63af97239bf62da7929b8
Alex <alex.cambrini@studio.unibo.it>
2025-03-25 21:17:09 +0100

    Refactor class to integrate with Hibernate

--- src/main/java/it/unibo/wastemaster/core/models/Person.java
+++ src/main/java/it/unibo/wastemaster/core/models/Person.java
@@ -1,17 +1,19 @@
 package it.unibo.wastemaster.core.models;
 
-public class Person {
-    protected int personId;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.CascadeType;
+
+@MappedSuperclass
+public abstract class Person {
     protected String name;
     protected String surname;
-    protected Location address;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Location address;
     protected String email;
     protected String phone;
 
-
-    // Constructor class Person
-    public Person(int personId, String name, String surname, Location address, String email, String phone) {
-        this.personId = personId;
+    public Person(String name, String surname, Location address, String email, String phone) {
         this.name = name;
         this.surname = surname;
         this.address = address;
@@ -19,9 +21,8 @@ public class Person {
         this.phone = phone;
     }
 
-    public int getId() {
-        return personId;
-    }
+    public Person() {
+    };
 
     public String getName() {
         return name;
@@ -30,12 +31,15 @@ public class Person {
     public String getSurname() {
         return surname;
     }
+
     public Location getAddress() {
         return address;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getPhone() {
         return phone;
     }
@@ -51,44 +55,17 @@ public class Person {
     public void setAddress(Location address) {
         this.address = address;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
     public String getInfo() {
-        return String.format("ID: %d, Name: %s, Surname: %s, Address: %s, Email: %s, Phone: %s", personId, name, surname, address, email, phone);
+        return String.format("Name: %s, Surname: %s, Address: %s, Email: %s, Phone: %s", name, surname, address, email,
+                phone);
     }
-
-    // //TEST
-    // public static void main(String[] args) {
-
-    //     Location location = new Location(0, "Via Roma", "10", "Milano", "Italy"); 
-
-    //     Person person = new Person(1, "Mario", location, "mario@example.com", "1234567890") {
-           
-    //     };
-   
-    //     System.out.println("Info iniziali: " + person.getInfo());
-    //     System.out.println("ID: " + person.getId());
-    //     System.out.println("Nome: " + person.getName());
-    //     System.out.println("Email: " + person.getEmail());
-    //     System.out.println("Telefono: " + person.getPhone());
-        
-
-    //     Location personAddress = person.getAddress();
-    //     System.out.println("Indirizzo: " + personAddress.getStreet() + " " + personAddress.getCivicNumber() + ", " + personAddress.getCity() + ", " + personAddress.getCity());
-    
-    //     // Modifica
-    //     person.setName("Giovanni");
-    //     person.setPhone("0987654321");
-    //     Location newLocation = new Location(1,"Via Milano", "5", "Torino", "Italy");
-    //     person.setAddress(newLocation);
-    
-    //     // Verifica
-    //     System.out.println("Info dopo la modifica: " + person.getInfo());
-    // }
-    
 }
