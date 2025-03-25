cfd2d82160a3ded2ae6988dcd27517ff7b3be2a3
Alex <alex.cambrini@studio.unibo.it>
2025-03-25 23:02:39 +0100

    Add test

--- src/main/java/it/unibo/wastemaster/main/App.java
+++ src/main/java/it/unibo/wastemaster/main/App.java
@@ -2,10 +2,19 @@ package it.unibo.wastemaster.main;
 
 import it.unibo.wastemaster.database.HibernateUtil;
 
+import it.unibo.wastemaster.core.models.Collection;
 import it.unibo.wastemaster.core.models.Customer;
 import it.unibo.wastemaster.core.models.Employee;
 import it.unibo.wastemaster.core.models.Location;
+import it.unibo.wastemaster.core.models.Reservation;
+import it.unibo.wastemaster.core.models.Reservation.ReservationStatus;
+import it.unibo.wastemaster.core.models.Waste;
+import it.unibo.wastemaster.core.models.Vehicle;
+
+
 import jakarta.persistence.EntityManager;
+import java.util.Date;
+
 
 public class App {
     public static void main(String[] args) {
@@ -21,9 +30,28 @@ public class App {
             Location location = new Location( "Via Roma", "10", "Milano", "Italy");
             Customer customer = new Customer("Mario", "Rossi", location, "mario@example.com", "1234567890");
             Employee employee = new Employee("Giuseppe", "Verdi", location, "giuseppe@example.com", "0987654321", Employee.Role.OPERATOR);
+            Collection wasteCollection  = new Collection(customer, new Date(), Waste.WasteType.PLASTIC, Collection.CollectionStatus.PENDING, 2, 1, false);
+
+
+            Date bookingDate = new Date();
+            Date pickupDate = new Date(System.currentTimeMillis() + 86400000L);
+
+            Reservation reservation = new Reservation(customer, bookingDate, pickupDate, ReservationStatus.PENDING);
+            
+            Vehicle vehicle = new Vehicle("ABe23CD", 1000, "Fiat", "Panda", 2010, Vehicle.LicenceType.C, Vehicle.VehicleStatus.IN_SERVICE);
+
+
+            Waste waste = new Waste(Waste.WasteType.PLASTIC, true, false);
+
+
             
             entityManager.persist(employee);
             entityManager.persist(customer);
+            entityManager.persist(wasteCollection);
+            entityManager.persist(reservation);
+            entityManager.persist(vehicle);
+            entityManager.persist(waste);
+
 
             entityManager.getTransaction().commit();
             
