b08d6ebc1dffa5d73dcbd56a25aff31271341d50
Alex <alex.cambrini@studio.unibo.it>
2025-03-25 21:21:00 +0100

    Add HikariCP dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,9 +50,16 @@ dependencies {
 
     // Hibernate Core per la gestione della persistenza
     implementation("org.hibernate:hibernate-core:6.2.0.Final")
-
+    implementation("org.hibernate.orm:hibernate-hikaricp:6.2.0.Final")
     // Driver JDBC per MySQL
     implementation("mysql:mysql-connector-java:8.0.33")
+
+    // HikariCP per la gestione del pool di connessioni
+    implementation("com.zaxxer:HikariCP:5.1.0")
+
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+
 }
 
 
