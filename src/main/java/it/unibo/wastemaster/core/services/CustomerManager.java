98c5a50e5237a243f359fdc934c040a2ee77d2f1
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-03-29 12:15:59 +0100

    Add fuction addCustomer

--- src/main/java/it/unibo/wastemaster/core/services/CustomerManager.java
+++ src/main/java/it/unibo/wastemaster/core/services/CustomerManager.java
@@ -3,6 +3,7 @@ package it.unibo.wastemaster.core.services;
 
 import it.unibo.wastemaster.core.dao.GenericDAO;
 import it.unibo.wastemaster.core.models.Customer;
+import it.unibo.wastemaster.core.models.Location;
 import jakarta.persistence.EntityManager;
 import it.unibo.wastemaster.database.HibernateUtil;
 
@@ -13,5 +14,11 @@ public class CustomerManager {
     public CustomerManager() {
         entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
         customerDAO = new GenericDAO<>(entityManager, Customer.class);
+    }
+
+    public void addCustomer(String name, String surname, String email, String phone, String street, String civicNumber, String city, String postalCode) {
+        Location location = new Location(street, civicNumber, city, postalCode);
+		Customer customer = new Customer(name, surname, location, email, phone);
+        customerDAO.insert(customer);
     }
 }
