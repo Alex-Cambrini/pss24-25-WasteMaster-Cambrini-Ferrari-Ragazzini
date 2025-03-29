4864643fce5cf425e33e70b4c42e94a1bd385e6e
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-03-29 12:08:09 +0100

    add costructor to CustomerManager file

--- src/main/java/it/unibo/wastemaster/core/services/CustomerManager.java
+++ src/main/java/it/unibo/wastemaster/core/services/CustomerManager.java
@@ -1,5 +1,17 @@
 package it.unibo.wastemaster.core.services;
 
+
+import it.unibo.wastemaster.core.dao.GenericDAO;
+import it.unibo.wastemaster.core.models.Customer;
+import jakarta.persistence.EntityManager;
+import it.unibo.wastemaster.database.HibernateUtil;
+
 public class CustomerManager {
+    private final GenericDAO<Customer> customerDAO;
+    private final EntityManager entityManager;
     
+    public CustomerManager() {
+        entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        customerDAO = new GenericDAO<>(entityManager, Customer.class);
+    }
 }
