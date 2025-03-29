f0e30e4e3ca0dc0650cd20fb382c618bf58bec7b
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-03-29 14:51:19 +0100

    initial class CustomerManagerTest

--- src/test/java/it/unibo/wastemaster/core/services/CustomerManagerTest.java
+++ src/test/java/it/unibo/wastemaster/core/services/CustomerManagerTest.java
@@ -1,5 +1,23 @@
 package it.unibo.wastemaster.core.services;
 
-public class CustomerManagerTest {
-    
+import it.unibo.wastemaster.core.models.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerManagerTest {
+
+	private CustomerManager customerManager;
+
+	@BeforeEach
+	void setUp() {
+		customerManager = new CustomerManager();
+	}
+
+	private String generateUniqueEmail(String prefix) {
+		return prefix + "_" + System.currentTimeMillis() + "@example.com";
+	}
+
+	
 }
