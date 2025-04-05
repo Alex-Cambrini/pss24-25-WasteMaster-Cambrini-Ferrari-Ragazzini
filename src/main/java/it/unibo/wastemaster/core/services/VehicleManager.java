880638bd01a62dfe0431b1470e8d01426cc545ff
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-05 14:14:48 +0200

    add initial structure for VehicleManager

--- src/main/java/it/unibo/wastemaster/core/services/VehicleManager.java
+++ src/main/java/it/unibo/wastemaster/core/services/VehicleManager.java
@@ -1,5 +1,16 @@
 package it.unibo.wastemaster.core.services;
 
+import it.unibo.wastemaster.core.dao.GenericDAO;
+import it.unibo.wastemaster.core.models.Vehicle;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
 public class VehicleManager {
-    
+    private final EntityManager em;
+	private final GenericDAO<Vehicle> vehicleDAO;
+
+	public VehicleManager(EntityManagerFactory emf) {
+		this.em = emf.createEntityManager();
+		this.vehicleDAO = new GenericDAO<>(em, Vehicle.class);
+	}
 }
