5195be61f414e68aa775a7995ffb4fe6a718073e
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-05 11:10:21 +0200

    fix transactionHelper

--- src/main/java/it/unibo/wastemaster/core/utils/TransactionHelper.java
+++ src/main/java/it/unibo/wastemaster/core/utils/TransactionHelper.java
@@ -3,16 +3,27 @@ package it.unibo.wastemaster.core.utils;
 import jakarta.persistence.EntityManager;
 
 public class TransactionHelper {
-    public static void executeTransaction(EntityManager entityManager, Runnable operation) {
-        try {
-            entityManager.getTransaction().begin();
-            operation.run();
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            if (entityManager.getTransaction().isActive()) {
-                entityManager.getTransaction().rollback();
-            }
-            throw e;
-        }
-    }
+
+	public static void executeTransaction(EntityManager entityManager, Runnable operation) {
+        
+		boolean startedHere = false;
+
+		try {
+			if (!entityManager.getTransaction().isActive()) {
+				entityManager.getTransaction().begin();
+				startedHere = true;
+			}
+
+			operation.run();
+
+			if (startedHere) {
+				entityManager.getTransaction().commit();
+			}
+		} catch (Exception e) {
+			if (entityManager.getTransaction().isActive()) {
+				entityManager.getTransaction().rollback();
+			}
+			throw e;
+		}
+	}
 }
