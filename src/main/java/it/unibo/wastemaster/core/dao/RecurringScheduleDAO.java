3919b9d42779b3e546ff980efb42ebb6c7aa47a0
Alex <alex.cambrini@studio.unibo.it>
2025-04-12 16:04:30 +0200

    Create RecurringScheduleDAO with method to find recurring schedules without future collections

---
+++ src/main/java/it/unibo/wastemaster/core/dao/RecurringScheduleDAO.java
@@ -0,0 +1,22 @@
+package it.unibo.wastemaster.core.dao;
+
+import java.util.List;
+
+import it.unibo.wastemaster.core.models.RecurringSchedule;
+import jakarta.persistence.EntityManager;
+
+public class RecurringScheduleDAO extends GenericDAO<RecurringSchedule> {
+
+    public RecurringScheduleDAO(EntityManager entityManager) {
+        super(entityManager, RecurringSchedule.class);
+    }
+
+    public List<RecurringSchedule> findRecurringSchedulesWithoutFutureCollections() {
+        String jpql = "SELECT rs FROM RecurringSchedule rs " +
+              "LEFT JOIN Collection c ON rs.id = c.schedule.id AND c.date > CURRENT_DATE " +
+              "WHERE c.schedule.id IS NULL";
+
+        return entityManager.createQuery(jpql, RecurringSchedule.class).getResultList();
+    }
+    
+}
