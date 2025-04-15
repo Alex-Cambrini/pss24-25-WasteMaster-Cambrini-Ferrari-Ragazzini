13fb42d9b4082254d186bf4264e20fe3b19ebd3f
Alex <alex.cambrini@studio.unibo.it>
2025-04-15 16:49:19 +0200

    Create OneTimeScheduleDao

---
+++ src/main/java/it/unibo/wastemaster/core/dao/OneTimeScheduleDAO.java
@@ -0,0 +1,19 @@
+package it.unibo.wastemaster.core.dao;
+
+import it.unibo.wastemaster.core.models.Collection;
+import it.unibo.wastemaster.core.models.OneTimeSchedule;
+import jakarta.persistence.EntityManager;
+
+public class OneTimeScheduleDAO extends GenericDAO<OneTimeSchedule> {
+
+    public OneTimeScheduleDAO(EntityManager entityManager) {
+        super(entityManager, OneTimeSchedule.class);
+    }
+
+    public Collection findCollectionByScheduleId(int scheduleId) {
+        return entityManager.createQuery(
+            "SELECT c FROM Collection c WHERE c.schedule.scheduleId = :scheduleId", Collection.class)
+            .setParameter("scheduleId", scheduleId)
+            .getSingleResult();
+    }
+}
