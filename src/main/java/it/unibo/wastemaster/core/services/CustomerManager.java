4cb5124b9cdb1560a499984cad164b1c6d59a0a4
Alex <alex.cambrini@studio.unibo.it>
2025-04-15 14:11:00 +0200

    Create CustomerDAO and move DB logic from service

--- src/main/java/it/unibo/wastemaster/core/services/CustomerManager.java
+++ src/main/java/it/unibo/wastemaster/core/services/CustomerManager.java
@@ -1,45 +1,30 @@
 package it.unibo.wastemaster.core.services;
 
 
-import it.unibo.wastemaster.core.dao.GenericDAO;
+import it.unibo.wastemaster.core.dao.CustomerDAO;
+
 import it.unibo.wastemaster.core.models.Customer;
 import it.unibo.wastemaster.core.models.Location;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import it.unibo.wastemaster.database.HibernateUtil;
+
 
 public class CustomerManager {
-    private final GenericDAO<Customer> customerDAO;
-    private final EntityManager entityManager;
-    
-    public CustomerManager() {
-	this(HibernateUtil.getEntityManagerFactory());
-    }
+    private CustomerDAO customerDAO;
 
-    public CustomerManager(EntityManagerFactory emf) {
-        this.entityManager = emf.createEntityManager();
-        this.customerDAO = new GenericDAO<>(entityManager, Customer.class);
+    public CustomerManager(CustomerDAO customerDAO) {
+        this.customerDAO = customerDAO;
     }
 
     public Customer addCustomer(String name, String surname, String email, String phone, String street, String civicNumber, String city, String postalCode) {
         if(name == null || name.isBlank() || email == null || email.isBlank()){
             throw new IllegalArgumentException("Nome ed email non possono essere vuoti");
         }
-        if(existsByEmail(email)){
+        if(customerDAO.existsByEmail(email)){
             throw new IllegalArgumentException("Un cliente con questa email esiste già");
         }
         Location location = new Location(street, civicNumber, city, postalCode);
         Customer customer = new Customer(name, surname, location, email, phone);
         customerDAO.insert(customer);
         return customer;
-    }
-
-    public boolean existsByEmail(String email) {
-        Long count = entityManager
-        .createQuery("SELECT COUNT(c) FROM Customer c WHERE c.email = :email", Long.class)
-        .setParameter("email", email)
-        .getSingleResult();
-        return count > 0;
     }
 
     public Customer getCustomerById(int customerId) {
