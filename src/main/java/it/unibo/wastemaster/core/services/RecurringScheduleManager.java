1a8619a38ab11ca3a5571cf4e12f60803cf668f7
Alex <alex.cambrini@studio.unibo.it>
2025-04-15 16:48:09 +0200

    Split ScheduleManager into two separate classes (OneTimeScheduleManager and RecurringScheduleManager)

--- src/main/java/it/unibo/wastemaster/core/services/RecurringScheduleManager.java
+++ src/main/java/it/unibo/wastemaster/core/services/RecurringScheduleManager.java
@@ -1,39 +1,32 @@
 package it.unibo.wastemaster.core.services;
 
-import it.unibo.wastemaster.core.dao.GenericDAO;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import it.unibo.wastemaster.core.dao.RecurringScheduleDAO;
 import it.unibo.wastemaster.core.models.Customer;
-import it.unibo.wastemaster.core.models.OneTimeSchedule;
 import it.unibo.wastemaster.core.models.RecurringSchedule;
 import it.unibo.wastemaster.core.models.RecurringSchedule.Frequency;
 import it.unibo.wastemaster.core.models.Schedule.ScheduleStatus;
 import it.unibo.wastemaster.core.models.Waste;
 import it.unibo.wastemaster.core.models.WasteSchedule;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+public class RecurringScheduleManager {
 
-public class ScheduleManager {
-
-    private WasteScheduleManager wasteScheduleManager;
     private RecurringScheduleDAO recurringScheduleDAO;
-    private GenericDAO<OneTimeSchedule> oneTimeScheduleDAO;
+    private WasteScheduleManager wasteScheduleManager;
 
-    public ScheduleManager(WasteScheduleManager wasteScheduleManager, RecurringScheduleDAO recurringScheduleDAO, GenericDAO<OneTimeSchedule> oneTimeScheduleDAO) {
+    public RecurringScheduleManager(RecurringScheduleDAO recurringScheduleDAO,
+            WasteScheduleManager wasteScheduleManager) {
         this.wasteScheduleManager = wasteScheduleManager;
         this.recurringScheduleDAO = recurringScheduleDAO;
-        this.oneTimeScheduleDAO = oneTimeScheduleDAO;
     }
 
-    public void createOneTimeSchedule(Customer customer, Waste.WasteType wasteType, ScheduleStatus status, Date pickupDate) {
-        OneTimeSchedule schedule = new OneTimeSchedule(customer, wasteType, status, pickupDate);
-        oneTimeScheduleDAO.insert(schedule);
-    }
-
-    public void createRecurringSchedule(Customer customer, Waste.WasteType wasteType, ScheduleStatus status, Frequency frequency) {
+    public void createRecurringSchedule(Customer customer, Waste.WasteType wasteType, ScheduleStatus status,
+            Frequency frequency) {
         RecurringSchedule schedule = new RecurringSchedule(customer, wasteType, status, frequency);
-        Date nextCollectionDate = calculateNextDate(schedule);        
+        Date nextCollectionDate = calculateNextDate(schedule);
         schedule.setNextCollectionDate(nextCollectionDate);
         recurringScheduleDAO.insert(schedule);
     }
@@ -43,20 +36,20 @@ public class ScheduleManager {
         RecurringSchedule.Frequency frequency = schedule.getFrequency();
         WasteSchedule scheduleData = wasteScheduleManager.getWasteScheduleForWaste(wasteType);
         int scheduledDay = scheduleData.getDayOfWeek();
-    
+
         Calendar calendar = Calendar.getInstance();
-    
+
         if (schedule.getNextCollectionDate() == null) {
             calendar.setTime(schedule.getCreationDate());
             calendar.add(Calendar.DAY_OF_MONTH, 2);
         } else {
             calendar.setTime(schedule.getNextCollectionDate());
         }
-    
+
         while (calendar.get(Calendar.DAY_OF_WEEK) != scheduledDay) {
             calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
-    
+
         if (schedule.getNextCollectionDate() != null) {
             if (frequency == RecurringSchedule.Frequency.WEEKLY) {
                 calendar.add(Calendar.DAY_OF_MONTH, 7);
@@ -64,12 +57,11 @@ public class ScheduleManager {
                 calendar.add(Calendar.DAY_OF_MONTH, 28);
             }
         }
-    
+
         return new java.sql.Date(calendar.getTimeInMillis());
     }
- 
+
     public List<RecurringSchedule> getRecurringSchedulesWithoutCollections() {
         return recurringScheduleDAO.findRecurringSchedulesWithoutFutureCollections();
     }
 }
-
