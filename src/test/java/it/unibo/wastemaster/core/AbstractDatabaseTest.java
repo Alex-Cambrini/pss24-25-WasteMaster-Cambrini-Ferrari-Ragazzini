d2e438b573441f48c40a1b60171959ce768c9405
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-16 18:48:12 +0200

    Add function setUp into BeforeEach for Customer and Location init

--- src/test/java/it/unibo/wastemaster/core/AbstractDatabaseTest.java
+++ src/test/java/it/unibo/wastemaster/core/AbstractDatabaseTest.java
@@ -1,5 +1,30 @@
 package it.unibo.wastemaster.core;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.wastemaster.core.models.Customer;
+import it.unibo.wastemaster.core.models.Location;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class AbstractDatabaseTest {
-    
+    protected EntityManagerFactory emf;
+	protected EntityManager em;
+
+	protected Location location;
+	protected Customer customer;
+
+	@BeforeEach
+	public void setUp() {
+		emf = Persistence.createEntityManagerFactory("test-pu");
+		em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		location = new Location("Via Roma", "10", "Bologna", "40100");
+		customer = new Customer("Mario", "Rossi", location, "mario.rossi@example.com", "1234567890");
+		em.persist(customer);
+
+		em.getTransaction().commit();
+	}
 }
