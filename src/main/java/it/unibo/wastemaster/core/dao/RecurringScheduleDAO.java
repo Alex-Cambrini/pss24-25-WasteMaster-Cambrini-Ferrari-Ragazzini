1b2af9dc603533ca492dbf3746f84772d3e9c337
Alex <alex.cambrini@studio.unibo.it>
2025-04-16 23:50:27 +0200

    UPDATE: rename method and update query ADD: add findActiveSchedulesWithNextDateBeforeToday method

--- src/main/java/it/unibo/wastemaster/core/dao/RecurringScheduleDAO.java
+++ src/main/java/it/unibo/wastemaster/core/dao/RecurringScheduleDAO.java
@@ -11,12 +11,21 @@ public class RecurringScheduleDAO extends GenericDAO<RecurringSchedule> {
         super(entityManager, RecurringSchedule.class);
     }
 
-    public List<RecurringSchedule> findRecurringSchedulesWithoutFutureCollections() {
+    public List<RecurringSchedule> findActiveSchedulesWithoutFutureCollections() {
         String jpql = "SELECT rs FROM RecurringSchedule rs " +
-                  "LEFT JOIN Collection c ON rs.id = c.schedule.id AND c.date > CURRENT_DATE " +
-                  "WHERE c.schedule.id IS NULL " +
-                  "AND rs.startDate <= CURRENT_DATE";    
+        "WHERE rs.nextCollectionDate > CURRENT_DATE " +
+        "AND rs.status = 'ACTIVE' " +
+        "AND NOT EXISTS (" +
+        "SELECT 1 FROM Collection c " +
+        "WHERE c.schedule = rs AND c.date > CURRENT_DATE" +
+        ")";
         return entityManager.createQuery(jpql, RecurringSchedule.class).getResultList();
     }
-    
+
+    public List<RecurringSchedule> findActiveSchedulesWithNextDateBeforeToday() {
+        String jpql = "SELECT rs FROM RecurringSchedule rs " +
+                "WHERE rs.status = 'ACTIVE' " +
+                "AND rs.nextCollectionDate < CURRENT_DATE";
+        return entityManager.createQuery(jpql, RecurringSchedule.class).getResultList();
+    }
 }
