c0df6b9a2d70b41fea3d95c55211cbdcb363e351
Alex <alex.cambrini@studio.unibo.it>
2025-04-16 18:30:49 +0200

    Update query with startDate

--- src/main/java/it/unibo/wastemaster/core/dao/RecurringScheduleDAO.java
+++ src/main/java/it/unibo/wastemaster/core/dao/RecurringScheduleDAO.java
@@ -13,9 +13,9 @@ public class RecurringScheduleDAO extends GenericDAO<RecurringSchedule> {
 
     public List<RecurringSchedule> findRecurringSchedulesWithoutFutureCollections() {
         String jpql = "SELECT rs FROM RecurringSchedule rs " +
-              "LEFT JOIN Collection c ON rs.id = c.schedule.id AND c.date > CURRENT_DATE " +
-              "WHERE c.schedule.id IS NULL";
-
+                  "LEFT JOIN Collection c ON rs.id = c.schedule.id AND c.date > CURRENT_DATE " +
+                  "WHERE c.schedule.id IS NULL " +
+                  "AND rs.startDate <= CURRENT_DATE";    
         return entityManager.createQuery(jpql, RecurringSchedule.class).getResultList();
     }
     
