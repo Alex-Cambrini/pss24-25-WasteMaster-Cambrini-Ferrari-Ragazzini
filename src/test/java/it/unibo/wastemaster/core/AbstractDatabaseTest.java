b2abbf5072fe2d00dcfd9fbc6af46a150332e6da
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-17 17:41:14 +0200

    Add to AbstractDatabaseTest initial DAO

--- src/test/java/it/unibo/wastemaster/core/AbstractDatabaseTest.java
+++ src/test/java/it/unibo/wastemaster/core/AbstractDatabaseTest.java
@@ -5,10 +5,17 @@ import java.util.Calendar;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import it.unibo.wastemaster.core.dao.CollectionDAO;
+import it.unibo.wastemaster.core.dao.CustomerDAO;
+import it.unibo.wastemaster.core.dao.GenericDAO;
+import it.unibo.wastemaster.core.dao.OneTimeScheduleDAO;
+import it.unibo.wastemaster.core.dao.RecurringScheduleDAO;
+import it.unibo.wastemaster.core.dao.WasteScheduleDAO;
 import it.unibo.wastemaster.core.models.Customer;
 import it.unibo.wastemaster.core.models.Location;
 import it.unibo.wastemaster.core.models.Waste;
 import it.unibo.wastemaster.core.models.WasteSchedule;
+import it.unibo.wastemaster.core.services.CustomerManager;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -22,25 +29,49 @@ public class AbstractDatabaseTest {
     protected Waste waste;
     protected WasteSchedule wasteSchedule;
 
+    protected CollectionDAO collectionDAO;
+    protected CustomerDAO customerDAO;
+    protected OneTimeScheduleDAO oneTimeScheduleDAO;
+    protected RecurringScheduleDAO recurringScheduleDAO;
+    protected WasteScheduleDAO wasteScheduleDAO;
+    protected CustomerManager customerManager;
+    protected <T> GenericDAO<T> createGenericDAO(Class<T> clazz) {
+        return new GenericDAO<>(em, clazz);
+    }
+    
+
+
 
 	@BeforeEach
-	public void setUp() {
-		emf = Persistence.createEntityManagerFactory("test-pu");
-		em = emf.createEntityManager();
-		em.getTransaction().begin();
+    public void setUp() {
+        emf = Persistence.createEntityManagerFactory("test-pu");
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
 
-		location = new Location("Via Roma", "10", "Bologna", "40100");
-		customer = new Customer("Mario", "Rossi", location, "mario.rossi@example.com", "1234567890");
+        location = new Location("Via Roma", "10", "Bologna", "40100");
+        em.persist(location);
+
+        customerDAO = new CustomerDAO(em);
+        customerManager = new CustomerManager(customerDAO);
+
+        customer = customerManager.addCustomer("Mario", "Rossi", "mario.rossi@example.com",
+            "1234567890", "Via Roma", "10", "Bologna", "40100");
+
         waste = new Waste(Waste.WasteType.PLASTIC, true, false);
         int scheduledDay = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
         wasteSchedule = new WasteSchedule(waste, scheduledDay);
-		em.persist(location);
-        em.persist(customer);
+
         em.persist(waste);
         em.persist(wasteSchedule);
 
-		em.getTransaction().commit();
-	}
+        em.getTransaction().commit();
+
+        collectionDAO = new CollectionDAO(em);
+        oneTimeScheduleDAO = new OneTimeScheduleDAO(em);
+        recurringScheduleDAO = new RecurringScheduleDAO(em);
+        wasteScheduleDAO = new WasteScheduleDAO(em);
+    }
+
 
     @AfterEach
 	public void tearDown() {
