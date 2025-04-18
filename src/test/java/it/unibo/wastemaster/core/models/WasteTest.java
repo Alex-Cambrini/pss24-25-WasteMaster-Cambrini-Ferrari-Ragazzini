9a405a17f8f2dd2364ce51ac1a937d01fcd9ea69
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-18 14:55:18 +0200

    Add unit tests for Waste class including getters, setters and toString

--- src/test/java/it/unibo/wastemaster/core/models/WasteTest.java
+++ src/test/java/it/unibo/wastemaster/core/models/WasteTest.java
@@ -1,5 +1,57 @@
 package it.unibo.wastemaster.core.models;
 
-public class WasteTest {
-    
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WasteTest {
+
+	private Waste waste;
+
+	@BeforeEach
+	public void setUp() {
+		waste = new Waste(Waste.WasteType.PLASTIC, true, false);
+	}
+
+	@Test
+	public void testGetType() {
+		assertEquals(Waste.WasteType.PLASTIC, waste.getType());
+	}
+
+	@Test
+	public void testSetType() {
+		waste.setType(Waste.WasteType.GLASS);
+		assertEquals(Waste.WasteType.GLASS, waste.getType());
+	}
+
+	@Test
+	public void testIsRecyclable() {
+		assertTrue(waste.getIsRecyclable());
+	}
+
+	@Test
+	public void testSetIsRecyclable() {
+		waste.setIsRecyclable(false);
+		assertFalse(waste.getIsRecyclable());
+	}
+
+	@Test
+	public void testIsDangerous() {
+		assertFalse(waste.getIsDangerous());
+	}
+
+	@Test
+	public void testSetIsDangerous() {
+		waste.setIsDangerous(true);
+		assertTrue(waste.getIsDangerous());
+	}
+
+	@Test
+	public void testToString() {
+		String str = waste.toString();
+		assertTrue(str.contains("PLASTIC"));
+		assertTrue(str.contains("true"));
+		assertTrue(str.contains("false"));
+	}
 }
