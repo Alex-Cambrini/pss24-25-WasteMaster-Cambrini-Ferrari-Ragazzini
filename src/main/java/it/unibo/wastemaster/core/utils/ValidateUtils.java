93c51b25927345da54ab04b68ec81648cdb1dc6d
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-22 16:21:08 +0200

    Add validation factory in ValidateUtils

--- src/main/java/it/unibo/wastemaster/core/utils/ValidateUtils.java
+++ src/main/java/it/unibo/wastemaster/core/utils/ValidateUtils.java
@@ -1,6 +1,18 @@
 package it.unibo.wastemaster.core.utils;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
 public class ValidateUtils {
+
+    public static final Validator VALIDATOR;
+
+    static {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        VALIDATOR = factory.getValidator();
+    }
+
     public static void validateString(String toValidate, String errorMessage) {
         if (toValidate == null || toValidate.isBlank()) {
             throw new IllegalArgumentException(errorMessage);
