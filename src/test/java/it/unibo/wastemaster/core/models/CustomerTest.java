772b37000c26dfa997724835165c52a5c1ca906f
Alex <alex.cambrini@studio.unibo.it>
2025-04-22 13:49:41 +0200

    Extend CustomerTest class and improve persistence testing

--- src/test/java/it/unibo/wastemaster/core/models/CustomerTest.java
+++ src/test/java/it/unibo/wastemaster/core/models/CustomerTest.java
@@ -2,43 +2,59 @@ package it.unibo.wastemaster.core.models;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import it.unibo.wastemaster.core.AbstractDatabaseTest;
+import it.unibo.wastemaster.core.models.Schedule.ScheduleStatus;
+import it.unibo.wastemaster.core.utils.DateUtils;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CustomerTest {
+public class CustomerTest extends AbstractDatabaseTest {
 
     private Customer customer;
     private Location location;
 
     @BeforeEach
     public void setUp() {
-        location = new Location("Via Roma", "1", "Milano", "Italy");
+        super.setUp();
+        location = new Location("Via Roma", "10", "Bologna", "40100");
         customer = new Customer("Mario", "Rossi", location, "mario.rossi@example.com", "1234567890");
     }
 
     @Test
-    public void testGetCustomerId() {
-        customer.setCustomerId(100);
-        assertEquals(100, customer.getCustomerId());
-    }
+    void testPersistenceAndGetter() {
+        em.getTransaction().begin();
+        em.persist(location);
+        em.persist(customer);
+        em.getTransaction().commit();
 
-    @Test
-    public void testSetCustomerId() {
-        customer.setCustomerId(200);
-        assertEquals(200, customer.getCustomerId());
+        Customer found = em.find(Customer.class, customer.getCustomerId());
+        assertNotNull(found);
+        assertEquals(customer.getName(), found.getName());
+
+        em.getTransaction().begin();
+        em.remove(found);
+        em.getTransaction().commit();
+
+        Customer deleted = em.find(Customer.class, customer.getCustomerId());
+        assertNull(deleted);
     }
 
     @Test
     public void testGetInfo() {
-        customer.setCustomerId(300);
+        em.getTransaction().begin();
+        em.persist(location);
+        em.persist(customer);
+        em.getTransaction().commit();
         String expectedInfo = String.format(
             "Name: %s, Surname: %s, Address: %s, Email: %s, Phone: %s, CustomerId: %d",
             "Mario", 
             "Rossi", 
-            location, 
+            location.toString(),
             "mario.rossi@example.com", 
             "1234567890", 
-            300
-        );
+            customer.getCustomerId()
+        );    
         assertEquals(expectedInfo, customer.getInfo());
-    }
+    }    
 }
