938482fe8fcc8e88ff49f7da353be5b183c11a31
manu4280572657 <manu.ragazzini04@gmail.com>
2025-04-23 19:18:41 +0200

    Added initial fields to Route class

--- src/main/java/it/unibo/wastemaster/core/models/Route.java
+++ src/main/java/it/unibo/wastemaster/core/models/Route.java
@@ -7,6 +7,15 @@ import java.util.List;
 
 public class Route {
 
-    
-  
+    private int id;
+    private Date departureDate;
+    private List<Location> stops;
+    private double estimatedDuration;
+    private String status;
+    private Schedule schedule;  
+
+   
+
 }
+  
+
