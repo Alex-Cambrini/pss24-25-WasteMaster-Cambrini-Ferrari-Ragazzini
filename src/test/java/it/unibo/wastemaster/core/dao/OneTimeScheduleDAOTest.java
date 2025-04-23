1717cba1e88cb1b52441404a190c7f741a327cd7
Alex <alex.cambrini@studio.unibo.it>
2025-04-23 20:54:28 +0200

    Create OneTimeScheduleDAOTest class for DAO testing and Fix query in OneTimeScheduleDAO class

---
+++ src/test/java/it/unibo/wastemaster/core/dao/OneTimeScheduleDAOTest.java
@@ -0,0 +1,70 @@
+package it.unibo.wastemaster.core.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.wastemaster.core.AbstractDatabaseTest;
+import it.unibo.wastemaster.core.models.Collection;
+import it.unibo.wastemaster.core.models.Customer;
+import it.unibo.wastemaster.core.models.Location;
+import it.unibo.wastemaster.core.models.OneTimeSchedule;
+import it.unibo.wastemaster.core.models.RecurringSchedule;
+import it.unibo.wastemaster.core.models.Waste;
+import it.unibo.wastemaster.core.utils.DateUtils;
+
+public class OneTimeScheduleDAOTest extends AbstractDatabaseTest {
+
+    private Location location;
+    private Customer customer;
+    private LocalDate date;
+    private Waste.WasteType wasteType;
+    private OneTimeSchedule oneTimeSchedule;
+    private RecurringSchedule recurringSchedule;
+    private Collection collection1;
+    private Collection collection2;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        date = DateUtils.getCurrentDate();
+        wasteType = Waste.WasteType.PLASTIC;
+
+        location = new Location("Via Roma", "10", "Bologna", "40100");
+        customer = new Customer("Mario", "Rossi", location, "mario.rossi@example.com", "1234567890");
+
+        oneTimeSchedule = new OneTimeSchedule(customer, wasteType, date);
+        recurringSchedule = new RecurringSchedule(customer, wasteType, date, RecurringSchedule.Frequency.WEEKLY);
+        
+        collection1 = new Collection(oneTimeSchedule);
+        collection2 = new Collection(recurringSchedule);
+
+        locationDAO.insert(location);
+        customerDAO.insert(customer);
+        oneTimeScheduleDAO.insert(oneTimeSchedule);
+        recurringScheduleDAO.insert(recurringSchedule);
+        collectionDAO.insert(collection1);
+        collection2.setCollectionDate(date);
+        collectionDAO.insert(collection2);
+    }
+
+    @Test
+    void testFindCollectionByScheduleId() {
+        int OneTimeScheduleId = collection1.getCollectionId();
+        int RecurringScheduleId = collection2.getCollectionId();
+        
+        Collection result1 = oneTimeScheduleDAO.findCollectionByScheduleId(OneTimeScheduleId);
+        Collection result2 = oneTimeScheduleDAO.findCollectionByScheduleId(RecurringScheduleId);
+
+        assertNotNull(result1);
+        assertNotNull(result2);
+        assertEquals(result1, collection1);
+        assertEquals(result2, collection2);
+    }
+
+}
