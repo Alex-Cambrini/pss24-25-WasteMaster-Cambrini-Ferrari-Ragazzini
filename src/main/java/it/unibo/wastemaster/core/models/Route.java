3405260f1431e40f20ec252a84cc4e01ee14b5ff
manu4280572657 <manu.ragazzini04@gmail.com>
2025-04-23 19:25:05 +0200

    Added constructor to Route class

--- src/main/java/it/unibo/wastemaster/core/models/Route.java
+++ src/main/java/it/unibo/wastemaster/core/models/Route.java
@@ -14,7 +14,14 @@ public class Route {
     private String status;
     private Schedule schedule;  
 
-   
+    public Route(int id, Date departureDate, List<Location> stops, double estimatedDuration, Schedule schedule) {
+        this.id = id;
+        this.departureDate = departureDate;
+        this.stops = stops;
+        this.estimatedDuration = estimatedDuration;
+        this.status = "In Progress";
+        this.schedule = schedule;
+    }
 
 }
   
