36518d421bb9bbd4748f13301758cf3af4a78f13
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-24 14:14:28 +0200

    Add test for findByWasteType in WasteScheduleDAO

--- src/test/java/it/unibo/wastemaster/core/dao/WasteScheduleDAOTest.java
+++ src/test/java/it/unibo/wastemaster/core/dao/WasteScheduleDAOTest.java
@@ -1,8 +1,12 @@
 package it.unibo.wastemaster.core.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.time.DayOfWeek;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import it.unibo.wastemaster.core.AbstractDatabaseTest;
 import it.unibo.wastemaster.core.models.Waste;
@@ -11,18 +15,27 @@ import it.unibo.wastemaster.core.models.WasteSchedule;
 public class WasteScheduleDAOTest extends AbstractDatabaseTest {
 
     private Waste plastic;
-	private WasteSchedule plasticSchedule;
+    private WasteSchedule plasticSchedule;
 
-	@BeforeEach
-	public void setUp() {
-		super.setUp();
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
 
-		plastic = new Waste(Waste.WasteType.PLASTIC, true, false);
-		plasticSchedule = new WasteSchedule(plastic, DayOfWeek.MONDAY);
+        plastic = new Waste(Waste.WasteType.PLASTIC, true, false);
+        plasticSchedule = new WasteSchedule(plastic, DayOfWeek.MONDAY);
 
-		em.getTransaction().begin();
-		em.persist(plastic);
-		wasteScheduleDAO.insert(plasticSchedule);
-		em.getTransaction().commit();
-	}
+        em.getTransaction().begin();
+        em.persist(plastic);
+        wasteScheduleDAO.insert(plasticSchedule);
+        em.getTransaction().commit();
+    }
+
+    @Test
+    void testFindByWasteType() {
+        WasteSchedule result = wasteScheduleDAO.findByWasteType(Waste.WasteType.PLASTIC);
+
+        assertNotNull(result);
+        assertEquals(plasticSchedule.getScheduleId(), result.getScheduleId());
+        assertEquals(Waste.WasteType.PLASTIC, result.getWaste().getType());
+    }
 }
