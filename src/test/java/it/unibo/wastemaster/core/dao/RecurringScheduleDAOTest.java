5fe8debb82fa98ea8584204d080c540f3be08731
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-24 11:08:31 +0200

    Add setUp function in RecurringScheduleDAOTest

--- src/test/java/it/unibo/wastemaster/core/dao/RecurringScheduleDAOTest.java
+++ src/test/java/it/unibo/wastemaster/core/dao/RecurringScheduleDAOTest.java
@@ -1,5 +1,35 @@
 package it.unibo.wastemaster.core.dao;
 
-public class RecurringScheduleDAOTest {
-    
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.wastemaster.core.AbstractDatabaseTest;
+import it.unibo.wastemaster.core.models.Customer;
+import it.unibo.wastemaster.core.models.Location;
+import it.unibo.wastemaster.core.models.RecurringSchedule;
+import it.unibo.wastemaster.core.models.Waste;
+import it.unibo.wastemaster.core.utils.DateUtils;
+
+public class RecurringScheduleDAOTest extends AbstractDatabaseTest {
+
+    private Location location;
+    private Customer customer;
+    private RecurringSchedule schedule;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        location = new Location("Via Roma", "10", "Bologna", "40100");
+        customer = new Customer("Mario", "Rossi", location, "mario.rossi@example.com", "1234567890");
+        schedule = new RecurringSchedule(
+                customer,
+                Waste.WasteType.PLASTIC,
+                DateUtils.getCurrentDate(),
+                RecurringSchedule.Frequency.WEEKLY);
+        schedule.setNextCollectionDate(DateUtils.getCurrentDate().plusDays(1));
+
+        locationDAO.insert(location);
+        customerDAO.insert(customer);
+        recurringScheduleDAO.insert(schedule);
+    }
+
 }
