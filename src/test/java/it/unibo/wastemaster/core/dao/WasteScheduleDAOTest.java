189a61286374886dec9a2e1be39284598d6d22b5
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-24 14:12:09 +0200

    Add setup for WasteScheduleDAOTest

--- src/test/java/it/unibo/wastemaster/core/dao/WasteScheduleDAOTest.java
+++ src/test/java/it/unibo/wastemaster/core/dao/WasteScheduleDAOTest.java
@@ -1,5 +1,28 @@
 package it.unibo.wastemaster.core.dao;
 
-public class WasteScheduleDAOTest {
-    
+import java.time.DayOfWeek;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.wastemaster.core.AbstractDatabaseTest;
+import it.unibo.wastemaster.core.models.Waste;
+import it.unibo.wastemaster.core.models.WasteSchedule;
+
+public class WasteScheduleDAOTest extends AbstractDatabaseTest {
+
+    private Waste plastic;
+	private WasteSchedule plasticSchedule;
+
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+
+		plastic = new Waste(Waste.WasteType.PLASTIC, true, false);
+		plasticSchedule = new WasteSchedule(plastic, DayOfWeek.MONDAY);
+
+		em.getTransaction().begin();
+		em.persist(plastic);
+		wasteScheduleDAO.insert(plasticSchedule);
+		em.getTransaction().commit();
+	}
 }
