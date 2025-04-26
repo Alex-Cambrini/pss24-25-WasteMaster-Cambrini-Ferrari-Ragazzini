ea601fa105bda26e6840788d3f3d06c79d01ab8c
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-26 12:18:32 +0200

    Add testFindByPlate in VehicleDAOTest class

--- src/test/java/it/unibo/wastemaster/core/dao/VehicleDAOTest.java
+++ src/test/java/it/unibo/wastemaster/core/dao/VehicleDAOTest.java
@@ -1,27 +1,46 @@
 package it.unibo.wastemaster.core.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import it.unibo.wastemaster.core.AbstractDatabaseTest;
 import it.unibo.wastemaster.core.models.Vehicle;
 
 public class VehicleDAOTest extends AbstractDatabaseTest {
-    
+
     private Vehicle v1;
-	private Vehicle v2;
-	private Vehicle v3;
+    private Vehicle v2;
+    private Vehicle v3;
 
-	@BeforeEach
-	public void setUp() {
-		super.setUp();
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
 
-		v1 = new Vehicle("AA111AA", "Iveco", "Daily", 2020, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
-		v2 = new Vehicle("BB222BB", "Mercedes", "Sprinter", 2021, Vehicle.LicenceType.C, Vehicle.VehicleStatus.IN_MAINTENANCE);
-		v3 = new Vehicle("CC333CC", "Fiat", "Ducato", 2022, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.OUT_OF_SERVICE);
+        v1 = new Vehicle("AA111AA", "Iveco", "Daily", 2020, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
+        v2 = new Vehicle("BB222BB", "Mercedes", "Sprinter", 2021, Vehicle.LicenceType.C,
+                Vehicle.VehicleStatus.IN_MAINTENANCE);
+        v3 = new Vehicle("CC333CC", "Fiat", "Ducato", 2022, Vehicle.LicenceType.C1,
+                Vehicle.VehicleStatus.OUT_OF_SERVICE);
 
-		vehicleDAO.insert(v1);
-		vehicleDAO.insert(v2);
-		vehicleDAO.insert(v3);
-	}
+        vehicleDAO.insert(v1);
+        vehicleDAO.insert(v2);
+        vehicleDAO.insert(v3);
+    }
+
+    @Test
+    public void testFindByPlate() {
+        Vehicle found = vehicleDAO.findByPlate("AA111AA");
+        assertNotNull(found);
+        assertEquals("Iveco", found.getBrand());
+        assertEquals(Vehicle.LicenceType.C1, found.getLicenceType());
+
+        Vehicle found2 = vehicleDAO.findByPlate("BB222BB");
+        assertNotNull(found2);
+        assertEquals("Mercedes", found2.getBrand());
+        assertEquals(Vehicle.LicenceType.C, found2.getLicenceType());
+    }
 
 }
