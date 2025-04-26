408407dcbf57fc28b4cbf56ef615b04f389eded3
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-26 11:41:54 +0200

    Add testVehicleGettersAndSetters function in VehicleTest class

--- src/test/java/it/unibo/wastemaster/core/models/VehicleTest.java
+++ src/test/java/it/unibo/wastemaster/core/models/VehicleTest.java
@@ -1,5 +1,41 @@
 package it.unibo.wastemaster.core.models;
 
-public class VehicleTest {
-    
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.wastemaster.core.AbstractDatabaseTest;
+
+public class VehicleTest extends AbstractDatabaseTest {
+
+    private Vehicle vehicle;
+
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+		vehicle = new Vehicle("AB123CD", "Iveco", "Daily", 2020, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
+	}
+
+    @Test
+	public void testVehicleGettersAndSetters() {
+		vehicle.setBrand("Mercedes");
+		vehicle.setModel("Sprinter");
+		vehicle.setRegistrationYear(2022);
+		vehicle.setLicenceType(Vehicle.LicenceType.C);
+		vehicle.setVehicleStatus(Vehicle.VehicleStatus.IN_MAINTENANCE);
+		LocalDate newDate = LocalDate.of(2024, 1, 1);
+		vehicle.setLastMaintenanceDate(newDate);
+
+		assertEquals("Mercedes", vehicle.getBrand());
+		assertEquals("Sprinter", vehicle.getModel());
+		assertEquals(2022, vehicle.getRegistrationYear());
+		assertEquals(Vehicle.LicenceType.C, vehicle.getLicenceType());
+		assertEquals(Vehicle.VehicleStatus.IN_MAINTENANCE, vehicle.getVehicleStatus());
+		assertEquals(newDate, vehicle.getLastMaintenanceDate());
+		assertEquals(2, vehicle.getCapacity()); // perché C -> 2
+	}
+
 }
