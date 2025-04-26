c171a3a9d6ff75fadf42a28dbaeaab7528cd591e
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-26 15:29:20 +0200

    Add BeforeEach and setUp function in VehicleManagerTest

--- src/test/java/it/unibo/wastemaster/core/services/VehicleManagerTest.java
+++ src/test/java/it/unibo/wastemaster/core/services/VehicleManagerTest.java
@@ -1,5 +1,30 @@
 package it.unibo.wastemaster.core.services;
 
-public class VehicleManagerTest {
-    
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.wastemaster.core.AbstractDatabaseTest;
+import it.unibo.wastemaster.core.models.Vehicle;
+
+public class VehicleManagerTest extends AbstractDatabaseTest {
+
+	private VehicleManager vehicleManager;
+	private Vehicle v1;
+	private Vehicle v2;
+	private Vehicle v3;
+
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+		vehicleManager = new VehicleManager(vehicleDAO);
+        em.getTransaction().begin();
+
+		v1 = new Vehicle("DD444DD", "Renault", "Master", 2023, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
+		v2 = new Vehicle("EE555EE", "MAN", "TGE", 2022, Vehicle.LicenceType.C, Vehicle.VehicleStatus.IN_MAINTENANCE);
+		v3 = new Vehicle("FF666FF", "Ford", "Transit", 2021, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.OUT_OF_SERVICE);
+
+		vehicleDAO.insert(v1);
+		vehicleDAO.insert(v2);
+		vehicleDAO.insert(v3);
+	}
+
 }
