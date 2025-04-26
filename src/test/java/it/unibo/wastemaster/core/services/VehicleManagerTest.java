7067ed5c905a89fa7e4fe87d8628782cf581e5c6
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-26 15:34:11 +0200

    Refactor with format document VehicleManagerTest

--- src/test/java/it/unibo/wastemaster/core/services/VehicleManagerTest.java
+++ src/test/java/it/unibo/wastemaster/core/services/VehicleManagerTest.java
@@ -17,81 +17,84 @@ import it.unibo.wastemaster.core.models.Vehicle;
 
 public class VehicleManagerTest extends AbstractDatabaseTest {
 
-	private VehicleManager vehicleManager;
-	private Vehicle v1;
-	private Vehicle v2;
-	private Vehicle v3;
+    private VehicleManager vehicleManager;
+    private Vehicle v1;
+    private Vehicle v2;
+    private Vehicle v3;
 
-	@BeforeEach
-	public void setUp() {
-		super.setUp();
-		vehicleManager = new VehicleManager(vehicleDAO);
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        vehicleManager = new VehicleManager(vehicleDAO);
         em.getTransaction().begin();
 
-		v1 = new Vehicle("DD444DD", "Renault", "Master", 2023, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
-		v2 = new Vehicle("EE555EE", "MAN", "TGE", 2022, Vehicle.LicenceType.C, Vehicle.VehicleStatus.IN_MAINTENANCE);
-		v3 = new Vehicle("FF666FF", "Ford", "Transit", 2021, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.OUT_OF_SERVICE);
+        v1 = new Vehicle("DD444DD", "Renault", "Master", 2023, Vehicle.LicenceType.C1,
+                Vehicle.VehicleStatus.IN_SERVICE);
+        v2 = new Vehicle("EE555EE", "MAN", "TGE", 2022, Vehicle.LicenceType.C, Vehicle.VehicleStatus.IN_MAINTENANCE);
+        v3 = new Vehicle("FF666FF", "Ford", "Transit", 2021, Vehicle.LicenceType.C1,
+                Vehicle.VehicleStatus.OUT_OF_SERVICE);
 
-		vehicleDAO.insert(v1);
-		vehicleDAO.insert(v2);
-		vehicleDAO.insert(v3);
-	}
-
-    @Test
-	public void testAddVehicleSuccess() {
-		Vehicle newVehicle = vehicleManager.addVehicle("GG777GG", "Volkswagen", "Crafter", 2024,
-				Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
-		assertNotNull(newVehicle);
-		assertEquals("Volkswagen", newVehicle.getBrand());
-	}
-
-	@Test
-	public void testAddVehicleDuplicatePlate() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			vehicleManager.addVehicle("DD444DD", "Fake", "Fake", 2023, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
-		});
-	}
+        vehicleDAO.insert(v1);
+        vehicleDAO.insert(v2);
+        vehicleDAO.insert(v3);
+    }
 
     @Test
-	public void testUpdateStatus() {
-		vehicleManager.updateStatus("DD444DD", Vehicle.VehicleStatus.IN_MAINTENANCE);
-		Vehicle updated = vehicleDAO.findByPlate("DD444DD");
-		assertEquals(Vehicle.VehicleStatus.IN_MAINTENANCE, updated.getVehicleStatus());
-	}
-
-	@Test
-	public void testUpdateStatusInvalidPlate() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			vehicleManager.updateStatus("ZZ999ZZ", Vehicle.VehicleStatus.IN_SERVICE);
-		});
-	}
+    public void testAddVehicleSuccess() {
+        Vehicle newVehicle = vehicleManager.addVehicle("GG777GG", "Volkswagen", "Crafter", 2024,
+                Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
+        assertNotNull(newVehicle);
+        assertEquals("Volkswagen", newVehicle.getBrand());
+    }
 
     @Test
-	public void testCanOperateVehicle() {
-		List<Vehicle.LicenceType> licences = List.of(Vehicle.LicenceType.C1, Vehicle.LicenceType.C);
-
-		assertTrue(vehicleManager.canOperateVehicle(v1, licences));
-		assertTrue(vehicleManager.canOperateVehicle(v2, List.of(Vehicle.LicenceType.C)));
-		assertFalse(vehicleManager.canOperateVehicle(v2, List.of(Vehicle.LicenceType.C1)));
-	}
-
-    @Test
-	public void testScheduleMaintenance() {
-		v1.setLastMaintenanceDate(LocalDate.now().minusYears(2));
-		vehicleDAO.update(v1);
-
-		vehicleManager.scheduleMaintenance();
-
-		Vehicle updated = vehicleDAO.findByPlate("DD444DD");
-		assertEquals(Vehicle.VehicleStatus.IN_MAINTENANCE, updated.getVehicleStatus());
-	}
+    public void testAddVehicleDuplicatePlate() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            vehicleManager.addVehicle("DD444DD", "Fake", "Fake", 2023, Vehicle.LicenceType.C1,
+                    Vehicle.VehicleStatus.IN_SERVICE);
+        });
+    }
 
     @Test
-	public void testCompleteMaintenance() {
-		vehicleManager.completeMaintenance("EE555EE");
+    public void testUpdateStatus() {
+        vehicleManager.updateStatus("DD444DD", Vehicle.VehicleStatus.IN_MAINTENANCE);
+        Vehicle updated = vehicleDAO.findByPlate("DD444DD");
+        assertEquals(Vehicle.VehicleStatus.IN_MAINTENANCE, updated.getVehicleStatus());
+    }
 
-		Vehicle updated = vehicleDAO.findByPlate("EE555EE");
-		assertEquals(Vehicle.VehicleStatus.IN_SERVICE, updated.getVehicleStatus());
-		assertEquals(LocalDate.now(), updated.getLastMaintenanceDate());
-	}
+    @Test
+    public void testUpdateStatusInvalidPlate() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            vehicleManager.updateStatus("ZZ999ZZ", Vehicle.VehicleStatus.IN_SERVICE);
+        });
+    }
+
+    @Test
+    public void testCanOperateVehicle() {
+        List<Vehicle.LicenceType> licences = List.of(Vehicle.LicenceType.C1, Vehicle.LicenceType.C);
+
+        assertTrue(vehicleManager.canOperateVehicle(v1, licences));
+        assertTrue(vehicleManager.canOperateVehicle(v2, List.of(Vehicle.LicenceType.C)));
+        assertFalse(vehicleManager.canOperateVehicle(v2, List.of(Vehicle.LicenceType.C1)));
+    }
+
+    @Test
+    public void testScheduleMaintenance() {
+        v1.setLastMaintenanceDate(LocalDate.now().minusYears(2));
+        vehicleDAO.update(v1);
+
+        vehicleManager.scheduleMaintenance();
+
+        Vehicle updated = vehicleDAO.findByPlate("DD444DD");
+        assertEquals(Vehicle.VehicleStatus.IN_MAINTENANCE, updated.getVehicleStatus());
+    }
+
+    @Test
+    public void testCompleteMaintenance() {
+        vehicleManager.completeMaintenance("EE555EE");
+
+        Vehicle updated = vehicleDAO.findByPlate("EE555EE");
+        assertEquals(Vehicle.VehicleStatus.IN_SERVICE, updated.getVehicleStatus());
+        assertEquals(LocalDate.now(), updated.getLastMaintenanceDate());
+    }
 }
