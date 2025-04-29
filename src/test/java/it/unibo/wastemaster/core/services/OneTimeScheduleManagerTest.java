b41de90570a9c685a9bd253f90f41cd527090782
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-29 11:54:10 +0200

    Add setUp function with BeforeEach in OneTimeScheduleManagerTest

--- src/test/java/it/unibo/wastemaster/core/services/OneTimeScheduleManagerTest.java
+++ src/test/java/it/unibo/wastemaster/core/services/OneTimeScheduleManagerTest.java
@@ -1,5 +1,35 @@
 package it.unibo.wastemaster.core.services;
 
-public class OneTimeScheduleManagerTest {
-    
+import it.unibo.wastemaster.core.AbstractDatabaseTest;
+import it.unibo.wastemaster.core.models.Customer;
+import it.unibo.wastemaster.core.models.Location;
+import it.unibo.wastemaster.core.models.Waste;
+
+import org.junit.jupiter.api.BeforeEach;
+
+
+
+class OneTimeScheduleManagerTest extends AbstractDatabaseTest {
+
+	private OneTimeScheduleManager oneTimeScheduleManager;
+	private Customer customer;
+	private Location location;
+	private Waste waste;
+
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+		em.getTransaction().begin();
+
+		location = new Location("Via Milano", "22", "Modena", "41100");
+		customer = new Customer("Luca", "Verdi", location, "luca.verdi@example.com", "3334567890");
+		waste = new Waste(Waste.WasteType.PLASTIC, true, false);
+
+		locationDAO.insert(location);
+		customerDAO.insert(customer);
+		wasteDAO.insert(waste);
+
+		oneTimeScheduleManager = new OneTimeScheduleManager(oneTimeScheduleDAO, collectionManager);
+	}
+
 }
