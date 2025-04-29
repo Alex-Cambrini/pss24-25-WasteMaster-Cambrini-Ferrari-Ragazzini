068c15fd208e97e00919afb43208c046012476c0
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-29 17:40:24 +0200

    Create MainController for initial JavaFX view management

---
+++ src/main/java/it/unibo/wastemaster/controller/MainController.java
@@ -0,0 +1,14 @@
+package it.unibo.wastemaster.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+	@FXML
+	private Label welcomeLabel;
+
+	@FXML
+	public void initialize() {
+	}
+}
