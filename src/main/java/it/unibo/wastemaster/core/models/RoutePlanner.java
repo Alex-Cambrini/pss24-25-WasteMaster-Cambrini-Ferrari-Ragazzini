f32bc9d0ae4a9a10af66fcf7d731673eb5c5a843
manu4280572657 <manu.ragazzini04@gmail.com>
2025-04-30 18:11:03 +0200

    Modified RoutePlanner and add schedules

--- src/main/java/it/unibo/wastemaster/core/models/RoutePlanner.java
+++ src/main/java/it/unibo/wastemaster/core/models/RoutePlanner.java
@@ -1,11 +1,15 @@
 package it.unibo.wastemaster.core.models;
 
+
+import java.util.*;
+
+
 public class RoutePlanner {
 
+    private Map<Long, Route> routes; 
+    private List<OneTimeSchedule> oneTimeSchedules;
+    private List<RecurringSchedule> recurringSchedules;
 
 
-
-
-    
-
+  
 }
