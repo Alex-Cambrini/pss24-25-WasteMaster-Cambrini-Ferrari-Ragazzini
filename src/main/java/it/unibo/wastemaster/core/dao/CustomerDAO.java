7422dd0ac6778b6aefe8728e06f72aeabc8d8009
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-04-30 19:47:17 +0200

    Add findCustomerDetails function in CustomerDAO

--- src/main/java/it/unibo/wastemaster/core/dao/CustomerDAO.java
+++ src/main/java/it/unibo/wastemaster/core/dao/CustomerDAO.java
@@ -1,5 +1,7 @@
 package it.unibo.wastemaster.core.dao;
 
+import java.util.List;
+
 import it.unibo.wastemaster.core.models.Customer;
 import jakarta.persistence.EntityManager;
 
@@ -17,4 +19,13 @@ public class CustomerDAO extends GenericDAO<Customer> {
         return count > 0;
     }
 
+    public List<Object[]> findCustomerDetails() {
+        return entityManager.createQuery("""
+            SELECT c.name, c.surname, c.email,
+                   l.street, l.civicNumber, l.city, l.postalCode
+            FROM Customer c
+            JOIN c.location l
+            WHERE c.isDeleted = false
+        """, Object[].class).getResultList();
+    }
 }
