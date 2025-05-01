5a97a52e84faf102eac46ff60de85a0abe1bc8f5
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-01 17:51:55 +0200

    Create RouteTest class

---
+++ src/test/java/it/unibo/wastemaster/core/models/RouteTest.java
@@ -0,0 +1,14 @@
+package it.unibo.wastemaster.core.models;
+
+
+import it.unibo.wastemaster.core.Route;
+import it.unibo.wastemaster.core.models.Location;
+import it.unibo.wastemaster.core.models.Schedule;
+import it.unibo.wastemaster.core.models.Vehicle;
+import org.junit.jupiter.api.Test;
+
+public class RouteTest {
+
+
+    
+}
