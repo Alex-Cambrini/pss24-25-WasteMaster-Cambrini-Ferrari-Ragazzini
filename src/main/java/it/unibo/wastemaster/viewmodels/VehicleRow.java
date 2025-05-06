9a46ff21771f229e633d07582f502bed796c4ef9
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-05-06 10:45:55 +0200

    Create VehicleRow class

---
+++ src/main/java/it/unibo/wastemaster/viewmodels/VehicleRow.java
@@ -0,0 +1,57 @@
+package it.unibo.wastemaster.viewmodels;
+
+public class VehicleRow {
+	private final String plate;
+	private final String brand;
+	private final String model;
+	private final int registrationYear;
+	private final String licenceType;
+	private final String vehicleStatus;
+	private final String lastMaintenanceDate;
+	private final String nextMaintenanceDate;
+
+	public VehicleRow(String plate, String brand, String model, int registrationYear,
+	                  String licenceType, String vehicleStatus,
+	                  String lastMaintenanceDate, String nextMaintenanceDate) {
+		this.plate = plate;
+		this.brand = brand;
+		this.model = model;
+		this.registrationYear = registrationYear;
+		this.licenceType = licenceType;
+		this.vehicleStatus = vehicleStatus;
+		this.lastMaintenanceDate = lastMaintenanceDate;
+		this.nextMaintenanceDate = nextMaintenanceDate;
+	}
+
+	public String getPlate() {
+		return plate;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public int getRegistrationYear() {
+		return registrationYear;
+	}
+
+	public String getLicenceType() {
+		return licenceType;
+	}
+
+	public String getVehicleStatus() {
+		return vehicleStatus;
+	}
+
+	public String getLastMaintenanceDate() {
+		return lastMaintenanceDate;
+	}
+
+	public String getNextMaintenanceDate() {
+		return nextMaintenanceDate;
+	}
+}
