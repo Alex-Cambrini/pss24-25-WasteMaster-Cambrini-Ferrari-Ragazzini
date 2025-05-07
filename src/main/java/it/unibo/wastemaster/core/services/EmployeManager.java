4e056eafa896c5174b0497265f8d6fd9db7d8742
Alex <alex.cambrini@studio.unibo.it>
2025-05-07 18:52:03 +0200

    Create EmployeManager and  EmployeeDAO with methods

---
+++ src/main/java/it/unibo/wastemaster/core/services/EmployeManager.java
@@ -0,0 +1,54 @@
+package it.unibo.wastemaster.core.services;
+
+import it.unibo.wastemaster.core.dao.EmployeeDAO;
+import it.unibo.wastemaster.core.models.Employee;
+import it.unibo.wastemaster.core.utils.ValidateUtils;
+
+public class EmployeManager {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeManager(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        if (isEmailRegistered(employee.getEmail())) {
+            throw new IllegalArgumentException(
+                    String.format("Cannot add employee: the email address '%s' is already in use.",
+                            employee.getEmail()));
+        }
+        employeeDAO.insert(employee);
+        return employee;
+    }
+
+    private boolean isEmailRegistered(String email) {
+        return employeeDAO.existsByEmail(email);
+    }
+
+    public Employee getEmployeeById(int employeeId) {
+        return employeeDAO.findById(employeeId);
+    }
+
+    public void updateEmployee(Employee toUpdateEmployee) {
+        ValidateUtils.validateEntity(toUpdateEmployee);
+        ValidateUtils.requireArgNotNull(toUpdateEmployee.getEmployeeId(), "Customer ID cannot be null");
+        Employee existing = employeeDAO.findByEmail(toUpdateEmployee.getEmail());
+        if (existing != null && !existing.getEmployeeId().equals(toUpdateEmployee.getEmployeeId())) {
+            throw new IllegalArgumentException("Email is already used by another employee.");
+        }
+        employeeDAO.update(toUpdateEmployee);
+    }
+
+    public boolean softDeleteCustomer(Employee employee) {
+        try {
+            ValidateUtils.requireArgNotNull(employee, "Employee cannot be null");
+            ValidateUtils.requireArgNotNull(employee.getEmployeeId(), "Employee ID cannot be null");
+            employee.delete();
+            updateEmployee(employee);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
