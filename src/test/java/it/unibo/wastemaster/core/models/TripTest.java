b2fda857a1a7cc1a66b2ae0f691fc9dd9e617467
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-07 22:48:51 +0200

    Modified trip fields ,import and setUp

---
+++ src/test/java/it/unibo/wastemaster/core/models/TripTest.java
@@ -0,0 +1,26 @@
+package it.unibo.wastemaster.core.models;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.wastemaster.core.models.Employee.LicenceType;
+import it.unibo.wastemaster.core.models.Employee.Role;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TripTest {
+
+    private Trip trip;
+    private Vehicle vehicle;
+    private List<Employee> operators;
+    private List<String> postalCodes;
+    private LocalDateTime departure;
+    private LocalDateTime returnTime;
+
+   
+}
