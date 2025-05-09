75558fbc65d56f62b8c5d1807f17e1f3467d03ec
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-09 17:25:46 +0200

    Added methods to find trips by operator and by vehicle

--- src/main/java/it/unibo/wastemaster/core/dao/TripDAO.java
+++ src/main/java/it/unibo/wastemaster/core/dao/TripDAO.java
@@ -3,7 +3,9 @@ package it.unibo.wastemaster.core.dao;
 
 import java.util.List;
 
+import it.unibo.wastemaster.core.models.Employee;
 import it.unibo.wastemaster.core.models.Trip;
+import it.unibo.wastemaster.core.models.Vehicle;
 import jakarta.persistence.EntityManager;
 
 public class TripDAO extends GenericDAO<Trip> {
@@ -30,5 +32,17 @@ public class TripDAO extends GenericDAO<Trip> {
             .orElse(null);
     }
 
+    public List<Trip> findByOperator(Employee operator) {
+    return entityManager.createQuery("SELECT t FROM Trip t JOIN t.operators o WHERE o = :operator", Trip.class)
+                        .setParameter("operator", operator)
+                        .getResultList();
+    }
+
+    public List<Trip> findByVehicle(Vehicle vehicle) {
+    return entityManager.createQuery("SELECT t FROM Trip t WHERE t.assignedVehicle = :vehicle", Trip.class)
+                        .setParameter("vehicle", vehicle)
+                        .getResultList();
+    }
+    
 }
 
