91685642033857b3e8fd7cb20f53564806438c11
Alex <alex.cambrini@studio.unibo.it>
2025-05-09 15:56:05 +0200

    Add methods to CollectionDAO and their respective tests in CollectionDAOTest: - findActiveCollectionByOneTimeSchedule - findCancelledCollectionsOneTimeSchedule

--- src/main/java/it/unibo/wastemaster/core/dao/CollectionDAO.java
+++ src/main/java/it/unibo/wastemaster/core/dao/CollectionDAO.java
@@ -3,17 +3,37 @@ package it.unibo.wastemaster.core.dao;
 import java.util.List;
 
 import it.unibo.wastemaster.core.models.Collection;
+import it.unibo.wastemaster.core.models.OneTimeSchedule;
 import jakarta.persistence.EntityManager;
 
 public class CollectionDAO extends GenericDAO<Collection> {
 
-            public CollectionDAO(EntityManager entityManager) {
+    public CollectionDAO(EntityManager entityManager) {
         super(entityManager, Collection.class);
     }
 
     public List<Collection> findCollectionByStatus(Collection.CollectionStatus status) {
-        return entityManager.createQuery("SELECT c FROM Collection c WHERE c.collectionStatus = :status", Collection.class)
+        return entityManager
+                .createQuery("SELECT c FROM Collection c WHERE c.collectionStatus = :status", Collection.class)
                 .setParameter("status", status)
+                .getResultList();
+    }
+
+    public Collection findActiveCollectionByOneTimeSchedule(OneTimeSchedule schedule) {
+        return entityManager.createQuery(
+                "SELECT c FROM Collection c WHERE c.schedule = :schedule AND c.collectionStatus != :cancelledStatus",
+                Collection.class)
+                .setParameter("schedule", schedule)
+                .setParameter("cancelledStatus", Collection.CollectionStatus.CANCELLED)
+                .getSingleResult();
+    }
+
+    public List<Collection> findCancelledCollectionsOneTimeSchedule(OneTimeSchedule schedule) {
+        return entityManager.createQuery(
+                "SELECT c FROM Collection c WHERE c.schedule = :schedule AND c.collectionStatus = :cancelledStatus",
+                Collection.class)
+                .setParameter("schedule", schedule)
+                .setParameter("cancelledStatus", Collection.CollectionStatus.CANCELLED)
                 .getResultList();
     }
 
