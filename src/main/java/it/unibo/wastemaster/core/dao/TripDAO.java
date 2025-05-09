716b14caf4257fddf2452f5eced0c5df06f5a24d
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-09 16:54:50 +0200

    Added TripDAO class to manage Trip entity operations

---
+++ src/main/java/it/unibo/wastemaster/core/dao/TripDAO.java
@@ -0,0 +1,17 @@
+package it.unibo.wastemaster.core.dao;
+
+
+import java.util.List;
+
+import it.unibo.wastemaster.core.models.Trip;
+import jakarta.persistence.EntityManager;
+
+public class TripDAO extends GenericDAO<Trip> {
+
+    public TripDAO(EntityManager entityManager) {
+        super(entityManager, Trip.class);
+    }
+
+   
+}
+
