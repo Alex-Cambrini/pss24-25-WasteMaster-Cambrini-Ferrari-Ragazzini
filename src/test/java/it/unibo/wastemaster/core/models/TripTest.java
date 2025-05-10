3de2c5041fd057442ca5e0be5a3032d288a8f810
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-10 17:26:54 +0200

    Set up basic TripTest structure with validation support

--- src/test/java/it/unibo/wastemaster/core/models/TripTest.java
+++ src/test/java/it/unibo/wastemaster/core/models/TripTest.java
@@ -1,10 +1,23 @@
 package it.unibo.wastemaster.core.models;
 
+import jakarta.validation.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.util.*;
 
-class TripTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-   
-   
+public class TripTest {
 
+    private Validator validator;
+
+    @BeforeEach
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    
 }
