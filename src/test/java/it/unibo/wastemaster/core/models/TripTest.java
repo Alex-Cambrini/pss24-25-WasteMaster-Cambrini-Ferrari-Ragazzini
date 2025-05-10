1391f024e61886746cc4fc3a4a6225d935f3cae9
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-10 17:59:56 +0200

    Add test for getter and setter methods in the Trip class

--- src/test/java/it/unibo/wastemaster/core/models/TripTest.java
+++ src/test/java/it/unibo/wastemaster/core/models/TripTest.java
@@ -22,18 +22,36 @@ public class TripTest extends AbstractDatabaseTest {
 
     @BeforeEach
     public void setUp() {
-    super.setUp();
-    vehicle = new Vehicle("AB123CD", "Iveco", "Daily", 2020, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
-    operator = new Employee("John", "Doe", new Location("Via Roma", "10", "Bologna", "40100"), "john.doe@example.com", "1234567890", Employee.Role.OPERATOR, Employee.LicenceType.C1);
-    departureTime = LocalDateTime.now().plusHours(1);
-    expectedReturnTime = departureTime.plusHours(5);
-    
-    assertNotNull(vehicle);
-    assertNotNull(operator);
-}
+        super.setUp();
+        vehicle = new Vehicle("AB123CD", "Iveco", "Daily", 2020, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
+        operator = new Employee("John", "Doe", new Location("Via Roma", "10", "Bologna", "40100"), "john.doe@example.com", "1234567890", Employee.Role.OPERATOR, Employee.LicenceType.C1);
+        departureTime = LocalDateTime.now().plusHours(1);
+        expectedReturnTime = departureTime.plusHours(5);
+        assertNotNull(vehicle);
+        assertNotNull(operator);
+    }
 
    
+    @Test
+        public void testGetterSetter() {
+        
+        trip = new Trip(1, "40100", vehicle, List.of(operator), departureTime, expectedReturnTime, Trip.TripStatus.PENDING, null);
+        
+        trip.setPostalCodes("40200");  
+        assertEquals("40200", trip.getPostalCodes()); 
 
+        trip.setDepartureTime(departureTime);
+        assertEquals(departureTime, trip.getDepartureTime());
+
+        trip.setExpectedReturnTime(expectedReturnTime);
+        assertEquals(expectedReturnTime, trip.getExpectedReturnTime());
+
+        trip.setStatus(Trip.TripStatus.COMPLETED);
+        assertEquals(Trip.TripStatus.COMPLETED, trip.getStatus());
+
+        trip.setOperators(List.of(operator));
+        assertEquals(1, trip.getOperators().size());
+    }
 
     
 
