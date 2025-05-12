39772b64c14d848ffaa2f8cdc7a2ca5b2c1a8351
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-12 17:41:07 +0200

    Remove unused imports from TripTest class

--- src/test/java/it/unibo/wastemaster/core/models/TripTest.java
+++ src/test/java/it/unibo/wastemaster/core/models/TripTest.java
@@ -2,15 +2,14 @@ package it.unibo.wastemaster.core.models;
 
 import it.unibo.wastemaster.core.AbstractDatabaseTest;
 import it.unibo.wastemaster.core.dao.TripDAO;
-import it.unibo.wastemaster.core.utils.ValidateUtils;
-import jakarta.validation.ConstraintViolation;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Set;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 public class TripTest extends AbstractDatabaseTest {
