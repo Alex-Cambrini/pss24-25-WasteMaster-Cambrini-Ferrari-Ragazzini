70e176c17a48af1dc4b60c994ec1e0df5241382c
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-12 17:53:35 +0200

    Set up unit test for TripManager

--- src/test/java/it/unibo/wastemaster/core/services/TripManagerTest.java
+++ src/test/java/it/unibo/wastemaster/core/services/TripManagerTest.java
@@ -2,6 +2,7 @@ package it.unibo.wastemaster.core.services;
 
 import it.unibo.wastemaster.core.AbstractDatabaseTest;
 import it.unibo.wastemaster.core.models.Employee;
+import it.unibo.wastemaster.core.models.Location;
 import it.unibo.wastemaster.core.models.Vehicle;
 import it.unibo.wastemaster.core.models.Trip;
 import jakarta.validation.ConstraintViolationException;
@@ -13,6 +14,23 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TripManagerTest {
+class TripManagerTest extends AbstractDatabaseTest {
 
+    private TripManager tripManager;
+    private Vehicle vehicle;
+    private Employee operator;
+    private LocalDateTime departureTime;
+    private LocalDateTime expectedReturnTime;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        vehicle = new Vehicle("AB123CD", "Iveco", "Daily", 2020, Vehicle.LicenceType.C1, Vehicle.VehicleStatus.IN_SERVICE);
+        operator = new Employee("John", "Doe", new Location("Via Roma", "10", "Bologna", "40100"), "john.doe@example.com", "1234567890", Employee.Role.OPERATOR, Employee.LicenceType.C1);
+        departureTime = LocalDateTime.now().plusHours(1);
+        expectedReturnTime = departureTime.plusHours(5);
+        tripManager = new TripManager(em);  
+    
+    
+    }
 }
