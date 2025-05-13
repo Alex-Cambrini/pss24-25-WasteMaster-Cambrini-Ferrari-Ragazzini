d516cf1db484592ef5bf3883404c3e99862c6efc
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-05-13 18:53:49 +0200

    Add dynamic customer autocomplete using TextField and ContextMenu in AddScheduleController

--- src/main/java/it/unibo/wastemaster/core/dao/OneTimeScheduleDAO.java
+++ src/main/java/it/unibo/wastemaster/core/dao/OneTimeScheduleDAO.java
@@ -1,6 +1,5 @@
 package it.unibo.wastemaster.core.dao;
 
-import it.unibo.wastemaster.core.models.Collection;
 import it.unibo.wastemaster.core.models.OneTimeSchedule;
 import jakarta.persistence.EntityManager;
 
