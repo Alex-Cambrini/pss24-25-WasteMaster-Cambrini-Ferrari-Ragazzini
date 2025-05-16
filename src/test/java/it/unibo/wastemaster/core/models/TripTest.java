08ccd315686b0a861cf40380cb0ee30bc667a0d4
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-16 18:15:58 +0200

    Remove unused import from TripTest class

--- src/test/java/it/unibo/wastemaster/core/models/TripTest.java
+++ src/test/java/it/unibo/wastemaster/core/models/TripTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
