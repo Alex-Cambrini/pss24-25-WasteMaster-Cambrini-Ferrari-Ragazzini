ba0ebff6d69c3e8c2041ee6e9cc777d1ce527811
manu4280572657 <manu.ragazzini04@gmail.com>
2025-05-17 21:33:30 +0200

    Create Invoice class with basic structure and necessary imports

---
+++ src/main/java/it/unibo/wastemaster/core/models/Invoice.java
@@ -0,0 +1,14 @@
+package it.unibo.wastemaster.core.models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+public class Invoice {
+
+}
