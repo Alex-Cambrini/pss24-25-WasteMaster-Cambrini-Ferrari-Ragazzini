ecd5cd49cf6cb4a34c01bb78ee43d05331190883
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-05-21 18:55:14 +0200

    Add existsByName methods in WasteDAO class

--- src/main/java/it/unibo/wastemaster/core/dao/WasteDAO.java
+++ src/main/java/it/unibo/wastemaster/core/dao/WasteDAO.java
@@ -3,9 +3,16 @@ package it.unibo.wastemaster.core.dao;
 import it.unibo.wastemaster.core.models.Waste;
 import jakarta.persistence.EntityManager;
 
-public class WasteDAO extends GenericDAO<Waste> {    
+public class WasteDAO extends GenericDAO<Waste> {
 
     public WasteDAO(EntityManager entityManager) {
         super(entityManager, Waste.class);
+    }
+
+    public boolean existsByName(String name) {
+        return entityManager
+                .createQuery("SELECT COUNT(w) FROM Waste w WHERE w.name = :name AND w.deleted = false", Long.class)
+                .setParameter("name", name)
+                .getSingleResult() > 0;
     }
 }
