2db5277b89813eebaf74b720cf24fde0c4df4b56
Alex <alex.cambrini@studio.unibo.it>
2025-05-22 12:32:21 +0200

    refactor DialogUtils to add modal support with CSS inheritance: - add showModalWithController method to open modal dialogs with controller access - create createSceneWithCss helper to apply CSS stylesheets from owner stage to new scenes - keep existing showError and showSuccess methods intact - improve UI consistency by ensuring modals inherit main CSS styles

--- src/main/java/it/unibo/wastemaster/controller/utils/DialogUtils.java
+++ src/main/java/it/unibo/wastemaster/controller/utils/DialogUtils.java
@@ -1,6 +1,15 @@
 package it.unibo.wastemaster.controller.utils;
 
+import java.io.IOException;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class DialogUtils {
 
@@ -19,4 +28,39 @@ public class DialogUtils {
 		alert.setContentText(message);
 		alert.showAndWait();
 	}
+
+	public static <T> Optional<T> showModalWithController(
+			String title,
+			String fxmlPath,
+			Stage owner,
+			Consumer<T> controllerInitializer) throws IOException {
+
+		FXMLLoader loader = new FXMLLoader(DialogUtils.class.getResource(fxmlPath));
+		Parent root = loader.load();
+
+		Stage dialogStage = new Stage();
+		dialogStage.setTitle(title);
+		dialogStage.initModality(Modality.APPLICATION_MODAL);
+		dialogStage.initOwner(owner);
+		Scene scene = createSceneWithCss(root, owner);
+		dialogStage.setScene(scene);
+
+		T controller = loader.getController();
+		controllerInitializer.accept(controller);
+
+		dialogStage.setOnShown(e -> {
+			dialogStage.setX(owner.getX() + (owner.getWidth() - dialogStage.getWidth()) / 2);
+			dialogStage.setY(owner.getY() + (owner.getHeight() - dialogStage.getHeight()) / 2);
+		});
+		dialogStage.showAndWait();
+
+		return Optional.ofNullable(controller);
+	}
+
+	public static Scene createSceneWithCss(Parent root, Stage owner) {
+		Scene scene = new Scene(root);
+		scene.getStylesheets().addAll(owner.getScene().getStylesheets());
+		return scene;
+	}
+
 }
