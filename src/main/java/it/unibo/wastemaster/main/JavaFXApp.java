87d4b39ca8cc217786e299bfcf8be362566fd14a
Alex <alex.cambrini@studio.unibo.it>
2025-05-22 15:12:52 +0200

    fix error handling in JavaFXApp: - remove DialogUtils usage in exception handling - print stack trace and error to stderr instead of showing dialog - set primaryStage as owner in AppContext before loading UI

--- src/main/java/it/unibo/wastemaster/main/JavaFXApp.java
+++ src/main/java/it/unibo/wastemaster/main/JavaFXApp.java
@@ -7,29 +7,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import it.unibo.wastemaster.controller.utils.DialogUtils;
 import it.unibo.wastemaster.core.context.AppContext;
 
 public class JavaFXApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-	try {
-		AppContext.init();
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/main/MainLayout.fxml"));
-		Parent root = loader.load();
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("/css/primer-light.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("WasteMaster");
-		primaryStage.setWidth(1000);
-		primaryStage.setHeight(700);
-		primaryStage.setMinWidth(1000);
-		primaryStage.setMinHeight(700);
-		primaryStage.show();
 
-	} catch (Exception e) {
-		DialogUtils.showError("Critical Error:\n", e.getMessage());
-		Platform.exit();
+		try {
+			AppContext.init();
+			AppContext.setOwner(primaryStage);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/main/MainLayout.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/css/primer-light.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("WasteMaster");
+			primaryStage.setWidth(1000);
+			primaryStage.setHeight(700);
+			primaryStage.setMinWidth(1000);
+			primaryStage.setMinHeight(700);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			System.err.println("Critical Error: " + e.getMessage());
+			e.printStackTrace();
+			Platform.exit();
+		}
 	}
-}}
+}
