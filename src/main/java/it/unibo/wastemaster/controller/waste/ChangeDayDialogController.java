5f12a11a7b60ce74d7c3c630fbee62599b0e1c9b
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-05-27 15:52:17 +0200

    Add many methods in ChangeDayDialogController class

--- src/main/java/it/unibo/wastemaster/controller/waste/ChangeDayDialogController.java
+++ src/main/java/it/unibo/wastemaster/controller/waste/ChangeDayDialogController.java
@@ -1,5 +1,34 @@
 package it.unibo.wastemaster.controller.waste;
 
+import it.unibo.wastemaster.core.models.WasteSchedule;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.time.DayOfWeek;
+import java.util.Arrays;
+
 public class ChangeDayDialogController {
-    
+
+    @FXML
+    private ComboBox<DayOfWeek> dayOfWeekComboBox;
+
+    private WasteSchedule schedule;
+
+    public void setSchedule(WasteSchedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public void setCurrentDay(DayOfWeek currentDay) {
+        dayOfWeekComboBox.getItems().setAll(Arrays.asList(DayOfWeek.values()));
+        dayOfWeekComboBox.getSelectionModel().select(currentDay);
+    }
+
+    public DayOfWeek getSelectedDay() {
+        return dayOfWeekComboBox.getSelectionModel().getSelectedItem();
+    }
+
+    @FXML
+    private void handleSave() {
+        dayOfWeekComboBox.getScene().getWindow().hide();
+    }
 }
