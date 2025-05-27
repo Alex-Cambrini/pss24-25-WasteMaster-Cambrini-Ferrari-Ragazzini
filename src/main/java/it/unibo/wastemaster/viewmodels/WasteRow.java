c99bbe36c9b3a9dfeb3306ce20ca2d6db5c0e402
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-05-27 11:58:11 +0200

    Add costructor and getter methods in WasteRow

--- src/main/java/it/unibo/wastemaster/viewmodels/WasteRow.java
+++ src/main/java/it/unibo/wastemaster/viewmodels/WasteRow.java
@@ -1,5 +1,36 @@
 package it.unibo.wastemaster.viewmodels;
 
+import java.time.DayOfWeek;
+
+import it.unibo.wastemaster.core.models.Waste;
+import it.unibo.wastemaster.core.models.WasteSchedule;
+
 public class WasteRow {
-    
+    private final String name;
+    private final boolean recyclable;
+    private final boolean dangerous;
+    private final DayOfWeek dayOfWeek;
+
+    public WasteRow(Waste waste, WasteSchedule schedule) {
+        this.name = waste.getWasteName();
+        this.recyclable = waste.getIsRecyclable();
+        this.dangerous = waste.getIsDangerous();
+        this.dayOfWeek = schedule != null ? schedule.getDayOfWeek() : null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isRecyclable() {
+        return recyclable;
+    }
+
+    public boolean isDangerous() {
+        return dangerous;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
 }
