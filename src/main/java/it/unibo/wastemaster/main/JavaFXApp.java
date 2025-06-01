f11e36e520ac5c627e2f59a13e44a556e6f4cbf8
Alex <alex.cambrini@studio.unibo.it>
2025-06-01 10:05:36 +0200

    refactor JavaFXApp to load login view on startup: - replace main layout with login layout - change window size and title to match login screen - reorder imports and remove unused ones

--- src/main/java/it/unibo/wastemaster/main/JavaFXApp.java
+++ src/main/java/it/unibo/wastemaster/main/JavaFXApp.java
@@ -1,42 +1,47 @@
 package it.unibo.wastemaster.main;
 
+import it.unibo.wastemaster.core.context.AppContext;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-
-
-
-import it.unibo.wastemaster.core.context.AppContext;
 
 public class JavaFXApp extends Application {
 
-	@Override
-	public void start(Stage primaryStage) {
+    private Parent root;
 
-		try {
-			AppContext.init();
-			AppContext.setOwner(primaryStage);
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/main/MainLayout.fxml"));
-			Parent root = loader.load();
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("/css/primer-light.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("WasteMaster");
-			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
-			primaryStage.setWidth(1000);
-			primaryStage.setHeight(700);
-			primaryStage.setMinWidth(1000);
-			primaryStage.setMinHeight(700);
-			primaryStage.show();
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AppContext.init();
+            AppContext.setOwner(primaryStage);
 
-		} catch (Exception e) {
-			System.err.println("Critical Error: " + e.getMessage());
-			e.printStackTrace();
-			Platform.exit();
-		}
-	}
+            FXMLLoader loader =
+                    new FXMLLoader(
+                            getClass().getResource("/layouts/login/LoginView.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/css/primer-light.css")
+                    .toExternalForm());
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("WasteMaster - Login");
+            primaryStage.getIcons()
+                    .add(new Image(getClass().getResourceAsStream("/images/icon.png")));
+            primaryStage.setWidth(400);
+            primaryStage.setHeight(300);
+            primaryStage.setMinWidth(400);
+            primaryStage.setMinHeight(300);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            System.err.println("Critical Error: " + e.getMessage());
+            e.printStackTrace();
+            Platform.exit();
+        }
+    }
 }
