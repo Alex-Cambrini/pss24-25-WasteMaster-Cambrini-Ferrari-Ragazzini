aed9231c0ad22f0e21dee3f4589c85c1d70b9d3c
manu4280572657 <manu.ragazzini04@gmail.com>
2025-06-06 17:00:00 +0200

    Add InvoiceManager class with constructor and DAO dependencies

--- src/main/java/it/unibo/wastemaster/core/services/InvoiceManager.java
+++ src/main/java/it/unibo/wastemaster/core/services/InvoiceManager.java
@@ -1,13 +1,29 @@
 package it.unibo.wastemaster.core.services;
 
+import it.unibo.wastemaster.core.dao.CollectionDAO;
 import it.unibo.wastemaster.core.dao.InvoiceDAO;
+import it.unibo.wastemaster.core.models.Collection;
+import it.unibo.wastemaster.core.models.Customer;
+import it.unibo.wastemaster.core.models.Invoice;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InvoiceManager {
 
     private final InvoiceDAO invoiceDAO;
+    private final CollectionDAO collectionDAO;
+    private static final double FIXED_FEE = 30.0;
 
-    public InvoiceManager(InvoiceDAO invoiceDAO) {
+    public InvoiceManager(InvoiceDAO invoiceDAO, CollectionDAO collectionDAO) {
         this.invoiceDAO = invoiceDAO;
+        this.collectionDAO = collectionDAO;
     }
+
+    
 
 }
