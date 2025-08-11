37268f4026a0038bd3571fc7205c8d6a309dbf05
Alex <alex.cambrini@studio.unibo.it>
2025-08-12 00:08:58 +0200

    feat: add findAll method to VehicleRepository, VehicleRepositoryImpl, and VehicleManager; reformat code

--- src/main/java/it/unibo/wastemaster/domain/repository/VehicleRepository.java
+++ src/main/java/it/unibo/wastemaster/domain/repository/VehicleRepository.java
@@ -1,11 +1,18 @@
 package it.unibo.wastemaster.domain.repository;
 
 import it.unibo.wastemaster.domain.model.Vehicle;
+import java.util.List;
 import java.util.Optional;
 
 public interface VehicleRepository {
+
     void save(Vehicle vehicle);
+
     Optional<Vehicle> findByPlate(String plate);
+
     void update(Vehicle vehicle);
+
     void delete(Vehicle vehicle);
+
+    List<Vehicle> findAll();
 }
