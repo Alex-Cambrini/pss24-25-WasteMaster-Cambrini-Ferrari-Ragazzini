f5e745dc88c0096ad338b3ddd46ef4e84e77cc35
manu4280572657 <manu.ragazzini04@gmail.com>
2025-08-31 16:21:51 +0200

    Create EditTripController class

---
+++ src/main/java/it/unibo/wastemaster/controller/trip/EditTripController.java
@@ -0,0 +1,61 @@
+package it.unibo.wastemaster.controller.trip;
+
+
+import it.unibo.wastemaster.domain.model.Employee;
+import it.unibo.wastemaster.domain.model.Trip;
+import it.unibo.wastemaster.domain.model.Vehicle;
+import it.unibo.wastemaster.domain.service.TripManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class EditTripController {
+
+    @FXML private TextField postalCodesField;
+    @FXML private ComboBox<Vehicle> vehicleCombo;
+    @FXML private ListView<Employee> operatorsList;
+    @FXML private DatePicker departureDate;
+    @FXML private DatePicker returnDate;
+    @FXML private Button saveButton;
+    @FXML private Button cancelButton;
+
+    private Trip tripToEdit;
+    private TripManager tripManager;
+    private TripController tripController;
+
+    public void setTripToEdit(Trip trip) {
+        this.tripToEdit = trip;
+        populateFields();
+    }
+
+    public void setTripManager(TripManager tripManager) {
+        this.tripManager = tripManager;
+    }
+
+    public void setTripController(TripController tripController) {
+        this.tripController = tripController;
+    }
+
+   
+
+    private void populateFields() {
+        if (tripToEdit == null) return;
+        postalCodesField.setText(tripToEdit.getPostalCodes());
+        vehicleCombo.setValue(tripToEdit.getAssignedVehicle());
+        operatorsList.getSelectionModel().clearSelection();
+        for (Employee op : tripToEdit.getOperators()) {
+            operatorsList.getSelectionModel().select(op);
+        }
+        if (tripToEdit.getDepartureTime() != null) {
+            departureDate.setValue(tripToEdit.getDepartureTime().toLocalDate());
+        }
+        if (tripToEdit.getExpectedReturnTime() != null) {
+            returnDate.setValue(tripToEdit.getExpectedReturnTime().toLocalDate());
+        }
+    }
+
+    
+}
