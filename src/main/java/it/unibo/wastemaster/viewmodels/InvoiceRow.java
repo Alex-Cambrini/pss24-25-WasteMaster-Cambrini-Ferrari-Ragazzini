d5a3c412ba80ee1dc7bcedca9bf7bff80c544f87
manu4280572657 <manu.ragazzini04@gmail.com>
2025-08-31 16:37:04 +0200

    Create InvoiceRow

---
+++ src/main/java/it/unibo/wastemaster/viewmodels/InvoiceRow.java
@@ -0,0 +1,48 @@
+package it.unibo.wastemaster.viewmodels;
+
+import it.unibo.wastemaster.domain.model.Invoice;
+import it.unibo.wastemaster.domain.model.Customer;
+
+/**
+ * ViewModel class that represents an invoice for the table view.
+ */
+public final class InvoiceRow {
+
+    private final Invoice invoice;
+    private final String id;
+    private final String customer;
+    private final String amount;
+    private final String status;
+
+    public InvoiceRow(final Invoice invoice) {
+        this.invoice = invoice;
+        this.id = String.valueOf(invoice.getInvoiceId());
+        this.customer = invoice.getCustomer() != null ? invoice.getCustomer().getName() : "";
+        this.amount = String.format("%.2f", invoice.getAmount());
+        this.status = invoice.getPaymentStatus() != null ? invoice.getPaymentStatus().toString() : "";
+    }
+
+     public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getIdAsLong() {
+        return invoice.getInvoiceId();
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
