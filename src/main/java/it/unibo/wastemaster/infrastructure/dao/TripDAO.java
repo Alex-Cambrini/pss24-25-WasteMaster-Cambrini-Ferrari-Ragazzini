6b6a1547aa3b47c22ee8990d64dcf47202bc3b66
manu4280572657 <manu.ragazzini04@gmail.com>
2025-09-18 14:04:16 +0200

    Add method to find collections by postal code

--- src/main/java/it/unibo/wastemaster/infrastructure/dao/TripDAO.java
+++ src/main/java/it/unibo/wastemaster/infrastructure/dao/TripDAO.java
@@ -1,5 +1,11 @@
 package it.unibo.wastemaster.infrastructure.dao;
 
+import java.util.List;
+
+
+
+
+import it.unibo.wastemaster.domain.model.Collection;
 import it.unibo.wastemaster.domain.model.Trip;
 import jakarta.persistence.EntityManager;
 
@@ -16,5 +22,20 @@ public class TripDAO extends GenericDAO<Trip> {
      */
     public TripDAO(final EntityManager entityManager) {
         super(entityManager, Trip.class);
+    }
+
+     /**
+     * Finds all collections by postal code (CAP).
+     *
+     * @param postalCode the postal code to filter collections
+     * @return a list of collections associated with the given postal code
+     */
+    
+       
+    public List<Collection> findCollectionsByPostalCode(String postalCode) {
+        String jpql = "SELECT c FROM Trip t JOIN t.collections c WHERE t.postalCode = :postalCode";
+        return getEntityManager().createQuery(jpql, Collection.class)
+                .setParameter("postalCode", postalCode)
+                .getResultList();
     }
 }
