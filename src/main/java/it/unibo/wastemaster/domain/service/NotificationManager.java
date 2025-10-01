f10ece71ba4c21e18c2e24b6e6eafcac42d1cc4c
Alex <alex.cambrini@studio.unibo.it>
2025-10-01 16:52:03 +0200

    feat: add NotificationManager repository dependencies

--- src/main/java/it/unibo/wastemaster/domain/service/NotificationManager.java
+++ src/main/java/it/unibo/wastemaster/domain/service/NotificationManager.java
@@ -1,5 +1,21 @@
 package it.unibo.wastemaster.domain.service;
 
+import it.unibo.wastemaster.domain.repository.CustomerRepository;
+import it.unibo.wastemaster.domain.repository.InvoiceRepository;
+import it.unibo.wastemaster.domain.repository.TripRepository;
+
 public class NotificationManager {
 
+
+    private final TripRepository tripRepository;
+    private final InvoiceRepository invoiceRepository;
+    private final CustomerRepository customerRepository;
+
+    private NotificationManager(TripRepository tripRepository,
+                                InvoiceRepository invoiceRepository,
+                                CustomerRepository customerRepository) {
+        this.tripRepository = tripRepository;
+        this.invoiceRepository = invoiceRepository;
+        this.customerRepository = customerRepository;
+        }
 }
