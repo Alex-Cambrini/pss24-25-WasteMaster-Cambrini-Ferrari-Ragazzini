88cbdce09b8f57e910964e9904baaae1b71673d3
Alex <alex.cambrini@studio.unibo.it>
2025-10-01 12:26:47 +0200

    refactor ServiceFactory and AbstractDatabaseTest: - add recurringScheduleManager dependency to TripManager constructor - update tripManager initialization to include recurringScheduleManager

--- src/test/java/it/unibo/wastemaster/infrastructure/AbstractDatabaseTest.java
+++ src/test/java/it/unibo/wastemaster/infrastructure/AbstractDatabaseTest.java
@@ -193,7 +193,7 @@ public abstract class AbstractDatabaseTest {
         oneTimeScheduleManager =
                 new OneTimeScheduleManager(oneTimeScheduleRepository, collectionManager);
         vehicleManager = new VehicleManager(vehicleRepository);
-        tripManager = new TripManager(tripRepository, collectionRepository);
+        tripManager = new TripManager(tripRepository, collectionRepository, recurringScheduleManager);
         invoiceManager = new InvoiceManager(invoiceRepository);
     }
 
